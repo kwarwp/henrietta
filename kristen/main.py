ae2e3475fcbb5e81254d02b9d2386716daf94673
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 10:53:12 -0300

    Saved kristen/main.py at Timestamp: 2018-04-18 13:53:12

--- kristen/main.py
+++ kristen/main.py
@@ -4,20 +4,30 @@ from _spy.vitollino.main import Cena, STYLE
 STYLE['width'] = 700
 
 cretaceo_n = "https://i.imgur.com/EwPWbms.jpg"
+cretaceo_o = "https://i.imgur.com/c9IK8Vm.jpg"
+cretaceo_s = "https://i.imgur.com/QFLIccY.jpg"
+cretaceo_l = "https://i.imgur.com/QFLIccY.jpg"
 
-class Cena1():
+class CenaCretaceo():
   def __init__(self):
     #from meredith.main import Cena2
-    self.cena1 = Cena(img = cretaceo_n)
+    self.cena_n = Cena(img = cretaceo_n)
+    self.cena_o = Cena(cretaceo_o, direita=self.cena_n)
+    self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
+    self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
+    esquerda = self.cena_n)
+    self.cena_n.esquerda = self.cena_o
+    self.cena_o.esquerda = self.cena_s
+    self.cena_s.esquerda = self.cena_l
     #cena2 = Cena2()
     #self.cena1.direita = cena2
 
-    self.cena1.vai()
+    self.cena_n.vai()
   def vai(self, *_):
-    self.cena1.vai()
+    self.cena_n.vai()
 
 def Game():
-  cena1 = Cena1()
+  cena1 = CenaCretaceo()
   cena1.vai()
 
 if __name__ == "__main__":
