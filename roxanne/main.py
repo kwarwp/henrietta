dd45dad24d75936f406ce795ae5c34b7ff31cf23
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:35:49 -0300

    Saved roxanne/main.py at Timestamp: 2018-04-18 14:35:49

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,1 +1,36 @@
-# henrietta.roxanne.main.py
+from _spy.vitollino.main import Cena, STYLE
+
+STYLE['width'] = 700
+
+entra_n = "https://i.imgur.com/gc2frjm.jpg"
+entra_nzoom = "https://i.imgur.com/ELvdLzp.jpg"
+entra_nabre = "https://i.imgur.com/u6ou7mM.jpg"
+entra_l = "https://i.imgur.com/F9Cem7k.jpg"
+entra_o = "https://i.imgur.com/gO8JFgU.jpg"
+entra_ozoom = "https://i.imgur.com/JBQY8wh.jpg"
+
+
+class Entrada():
+   def __init__ (self):
+      self.cena_n = (img= entra_n)
+      self.cena_o = Cena(img = entra_o, direita=self.cena_n, 
+      esquerda = self.cena_nz)    
+      self.cena_na = Cena(img = entra_nabre)
+      self.cena_nz = Cena(img = entra_nzoom)
+      self.cena_n = Cena(img = entra_n, direita =self.cena_l, meio = self)
+      self.cena_l  =  Cena(img= entra_l,esquerda = self.cena_n)
+      
+      self.cena_n.esquerda = self.cena_o
+      self.cena_o.esquerda = self.cena_s
+      self.cena_s.esquerda = self.cena_l
+        
+        
+        
+        
+        
+def Game():
+  cena1 = Entrada()
+  cena1.vai()
+
+if __name__ == "__main__":
+	Game()
