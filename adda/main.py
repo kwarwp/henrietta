d9f74a2b538bb0704c1928a8a678797d9821ddb3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:47:25 -0300

    Saved adda/main.py at Timestamp: 2018-04-18 14:47:25

--- adda/main.py
+++ adda/main.py
@@ -12,7 +12,7 @@ class CenaFrutosDaTerra():
   def __init__(self):
     self.cena_f = Cena(img = FrutosDaTerra)
     self.cena_c = Cena(Calcita, direita=self.cena_f)
-    self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
+    self.cena_s  = Cena(cretaceo_s, direita=self.cena_f)
     #self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
     #esquerda = self.cena_n)
     self.cena_f = self.cena_c
