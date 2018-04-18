fa31a6f3b66ea1b7f3392657f258133fec45997a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:32:50 -0300

    Saved adda/main.py at Timestamp: 2018-04-18 14:32:50

--- adda/main.py
+++ adda/main.py
@@ -3,22 +3,22 @@ from _spy.vitollino.main import Cena, STYLE
 
 #STYLE['width'] = 700
 
-cretaceo_s = "https://i.imgur.com/FV4goWj.jpg"
-cretaceo_o = "https://i.imgur.com/FV4goWj.jpg"
+#cretaceo_s = "https://i.imgur.com/FV4goWj.jpg"
+#cretaceo_o = "https://i.imgur.com/FV4goWj.jpg"
 cretaceo_n = "https://i.imgur.com/FV4goWj.jpg"
-cretaceo_l = "https://i.imgur.com/FV4goWj.jpg"
+#cretaceo_l = "https://i.imgur.com/FV4goWj.jpg"
 
 class CenaFrutosDaTerra():
   def __init__(self):
     self.cena_n = Cena(img = cretaceo_n)
-    self.cena_o = Cena(cretaceo_o, direita=self.cena_n)
-    self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
-    self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
-    esquerda = self.cena_n)
-    self.cena_n.esquerda = self.cena_o
-    self.cena_n.direita = self.cena_l
-    self.cena_o.esquerda = self.cena_s
-    self.cena_s.esquerda = self.cena_l
+    #self.cena_o = Cena(cretaceo_o, direita=self.cena_n)
+    #self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
+    #self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
+    #esquerda = self.cena_n)
+    #self.cena_n.esquerda = self.cena_o
+    #self.cena_n.direita = self.cena_l
+    #self.cena_o.esquerda = self.cena_s
+    #self.cena_s.esquerda = self.cena_l
     #cena2 = Cena2()
     #self.cena1.direita = cena2
 
