0a777b65f4d471b5194ac20a79a7bcc59f783a52
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:21:32 -0300

    Saved anastasia/main.py at Timestamp: 2018-04-18 14:21:32

--- anastasia/main.py
+++ anastasia/main.py
@@ -17,7 +17,7 @@ class CenaAnhanguera():
     self.cenavoa.esquerda = self.cenacabeca
     self.cenavoa.direita = self.cenalibelula
     
-     self.cenavoa.vai()
+    self.cenavoa.vai()
   def vai(self, *_):
     self.cenavoa.vai()
     
