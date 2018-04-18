3ecc87566dd54f798216e27ae9ce525b55a5ec6d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:23:53 -0300

    Saved sarah/main.py at Timestamp: 2018-04-18 14:23:53

--- sarah/main.py
+++ sarah/main.py
@@ -1,5 +1,7 @@
 # henrietta.sarah.main.py
-from _spy.vitollino.main import Cena
+from _spy.vitollino.main import Cena, STYLE
+
+STYLE['width'] = 700
 
 salapedras1 = "https://i.imgur.com/2kCYcjs.jpg"
 salapedras2 = "https://i.imgur.com/AkcB954.jpg"
@@ -7,7 +9,6 @@ salapedras3 = "https://i.imgur.com/AXT4Zcl.jpg%20https://i.imgur.com/mr58OsA.jpg
 
 class Cenasalapedras():
     def __init__(self):
-        #from Lisa.main import CenaDino
         self.cena1 = Cena(img = salapedras1)
         self.cena2 = Cena(img = salapedras2, direita=self.cena1)
         self.cena3 = Cena(img = salapedras3, direita=self.cena2,
@@ -16,17 +17,20 @@ class Cenasalapedras():
         self.cena2.esquerda = self.cena3
     
     
-    
+
         self.cena1.vai()
     def vai(self,*_):
-    self.cena1.vai()
+        self.cena1.vai()
     def vai_sul(self, *_):
         from CenaDino.main import CenaDino
         CenaDino().vai()
      
-     def Game():
-cena
-  
+    def Game():
+    cena_1 = Cenasalapedras()
+    cena_1.vai()
+
+if __name__ == "__main__":
+	Game()
     
     
     
