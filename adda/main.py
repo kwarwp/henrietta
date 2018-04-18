b4c433beca578bfd087af81a879a9298cd30922d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 10:55:50 -0300

    Saved adda/main.py at Timestamp: 2018-04-18 13:55:49

--- adda/main.py
+++ adda/main.py
@@ -1,1 +1,12 @@
 # henrietta.adda.main.py
+from _spy.vitollino.main import Cena, STYLE
+
+STYLE['width'] = 700
+
+#colocar as imagens 
+frutosDaTerra_n = "http://bit.ly/2FOvGTr"
+
+class CenaFrutosDaTerra():
+__def __init__(self):
+	#from meredith.main import Cena2
+    self.cena_n = Cena(img = frutosDaTerra_n)
