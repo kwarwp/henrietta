d3a56cab2b51fd3bf0e797b122a871d50c49c17d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:22:12 -0300

    Saved morgan/main.py at Timestamp: 2018-04-18 14:22:12

--- morgan/main.py
+++ morgan/main.py
@@ -1,1 +1,23 @@
 # henrietta.morgan.main.py
+from _spy.vitollino.main import Cena, STYLE
+
+STYLE['width'] = 700
+
+parede1 = "https://i.imgur.com/Yn75ZF2.jpg
+parede2 = "https://i.imgur.com/4Xx7uox.jpg
+parede3 = "https://i.imgur.com/icYyd5H.jpg
+
+class Cenatectonica():
+ def __init__(self):
+
+    self.parede1 = Cena(img = parede1)
+    self.parede2 = Cena(parede2, direita=self.parede1)
+    self.parede3 = Cena(parede3, direita=self.parede2)
+    
+    def Game():
+  cena1 = Cenatectonica
+  cena1.vai()
+
+if __name__ == "__main__":
+	Game()
+    
