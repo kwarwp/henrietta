f6a1e9c40b8d1d8d4ba4fb0350c9ca0e7049b297
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:51:53 -0300

    Saved adda/main.py at Timestamp: 2018-04-18 14:51:52

--- adda/main.py
+++ adda/main.py
@@ -3,8 +3,6 @@ from _spy.vitollino.main import Cena, STYLE
 
 STYLE['width'] = 700
 
-#cretaceo_s = "https://i.imgur.com/FV4goWj.jpg"
-#cretaceo_o = "https://i.imgur.com/FV4goWj.jpg"
 FrutosDaTerra = "https://i.imgur.com/FV4goWj.jpg"
 Calcita = "https://i.imgur.com/biw6bwh.jpg"
 
@@ -12,16 +10,8 @@ class CenaFrutosDaTerra():
   def __init__(self):
     self.cena_f = Cena(img = FrutosDaTerra)
     self.cena_c = Cena(Calcita, direita=self.cena_f)
-    #self.cena_s  = Cena(cretaceo_s, direita=self.cena_f)
-    #self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
-    #esquerda = self.cena_n)
     self.cena_f = self.cena_c
-    #self.cena_s.meio = Cena(vai=self.vai_sul)
-    #self.cena_n.direita = self.cena_l
-    #self.cena_o.esquerda = self.cena_s
-    #self.cena_s.esquerda = self.cena_l
-    #cena2 = Cena2()
-    #self.cena1.direita = cena2
+    self.cena_f.meio = Cena(vai=self.vai_sul)
 
     self.cena_f.vai()
   def vai(self, *_):
