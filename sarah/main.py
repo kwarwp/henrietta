62517fa64c7e2fea7a4d7b1a1bf2a09d82806265
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:07:10 -0300

    Saved sarah/main.py at Timestamp: 2018-04-18 14:07:09

--- sarah/main.py
+++ sarah/main.py
@@ -7,12 +7,28 @@ salapedras3 = "https://i.imgur.com/AXT4Zcl.jpg%20https://i.imgur.com/mr58OsA.jpg
 
 class Cenasalapedras():
     def __init__(self):
-    self.cena1 = Cena(img = salapedras1)
-    self.cena2 = Cena(img = salapedras2, direita=self.cena1)
-    self.cena3 = Cena(img = salapedras3, direita=self.cena2,
-    esquerda = self.cena1)
-    self.cena1.esquerda = self.cena2
-    self.cena2.esquerda = self.cena3
+        #from Lisa.main import CenaDino
+        self.cena1 = Cena(img = salapedras1)
+        self.cena2 = Cena(img = salapedras2, direita=self.cena1)
+        self.cena3 = Cena(img = salapedras3, direita=self.cena2,
+           esquerda = self.cena1)
+        self.cena1.esquerda = self.cena2
+        self.cena2.esquerda = self.cena3
+    
+    
+    
+    self.cena1.vai()
+   def vai(self,*_):
+    self.cena1.vai()
+   def vai_sul(self, *_):
+     from CenaDino.main import CenaDino
+     CenaDino().vai()
+     
+def Game():
+cena
+  
+    
+    
     
     
     
