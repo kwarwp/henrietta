b0f7417339961176ed71d55726852cd7d4767736
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:19:08 -0300

    Saved lisa/main.py at Timestamp: 2018-04-18 14:19:07

--- lisa/main.py
+++ lisa/main.py
@@ -1,1 +1,28 @@
 # henrietta.lisa.main.py
+from _spy.vitollino.main import Cena, STYLE
+
+STYLE ['width'] = 700
+
+dino_direita = "https://i.imgur.com/P6nIihH.jpg"
+dino_esquerda = "https://i.imgur.com/P54FLEU.jpg"
+
+class CenaDino():
+    def __init__(self):
+    #from meredith.main import Cena2
+     self.cena_direita = Cena(img = dino_direita)
+     self.cena_esquerda = Cena(img = dino_esquerda, direita=self.cena_direita)
+     self.cena_direita.esquerda = self.cena_esquerda
+     self.cena_direita.direita = self.cena_esquerda
+     self.cena_esquerda.direita = self.cena_direita
+     self.cena_esquerda.esquerda = self.cena_direita
+    #cena2 = Cena2()
+    #self.cena1.direita = cena2
+    
+    
+    
+def sala():
+  cena_dino = CenaDino()
+  cena_dino.vai()
+  
+  if __name__ == "__main__":
+  	do_dino
