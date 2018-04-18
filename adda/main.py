8abc7d6157221a37e8f8988decfbe811d26b8a08
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:21:43 -0300

    Saved adda/main.py at Timestamp: 2018-04-18 14:21:43

--- adda/main.py
+++ adda/main.py
@@ -2,34 +2,36 @@
 from _spy.vitollino.main import Cena, STYLE
 
 STYLE['width'] = 700
-#colocar as imagens 
-frutosDaTerra_n = "https://drive.google.com/open?id=1HigI3FQMSNJnskLqi30cvTxndG6_CLSE"
-#cretaceo_n = "https://i.imgur.com/EwPWbms.jpg"
-#cretaceo_o = "https://i.imgur.com/c9lK8Vm.jpg"
-#cretaceo_s = "https://i.imgur.com/QFLIccY.jpg"
-#cretaceo_l = "https://i.imgur.com/K1LChZw.jpg"
+
+cretaceo_s = "https://i.imgur.com/EwPWbms.jpg"
+cretaceo_o = "https://i.imgur.com/c9lK8Vm.jpg"
+cretaceo_n = "https://i.imgur.com/QFLlccY.jpg"
+cretaceo_l = "https://i.imgur.com/K1LChZw.jpg"
 
 class CenaCretaceo():
   def __init__(self):
-    #from meredith.main import Cena2
-    self.cena_n = Cena(img = frutosDaTerra_n)
-    #self.cena_o = Cena(cretaceo_o, direita=self.cena_n)
-    #self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
-    #self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
-    #esquerda = self.cena_n)
-    #self.cena_n.esquerda = self.cena_o
-    #self.cena_o.esquerda = self.cena_s
-    #self.cena_s.esquerda = self.cena_l
+    self.cena_n = Cena(img = cretaceo_n)
+    self.cena_o = Cena(cretaceo_o, direita=self.cena_n)
+    self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
+    self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
+    esquerda = self.cena_n)
+    self.cena_n.esquerda = self.cena_o
+    self.cena_n.direita = self.cena_l
+    self.cena_o.esquerda = self.cena_s
+    self.cena_s.esquerda = self.cena_l
     #cena2 = Cena2()
     #self.cena1.direita = cena2
 
     self.cena_n.vai()
   def vai(self, *_):
     self.cena_n.vai()
+  def vai_sul(self, *_):
+    from anastasia.main import CenaAnhanguera
+    CenaAnhanguera().vai()
 
 def Game():
-  cena_n = CenaCretaceo()
-  cena_n.vai()
+  cena1 = CenaCretaceo()
+  cena1.vai()
 
 if __name__ == "__main__":
 	Game()
