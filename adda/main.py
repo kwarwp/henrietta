f99de4e0c637125f7961a1aa39e4209f09ecd55a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:46:42 -0300

    Saved adda/main.py at Timestamp: 2018-04-18 14:46:41

--- adda/main.py
+++ adda/main.py
@@ -12,10 +12,11 @@ class CenaFrutosDaTerra():
   def __init__(self):
     self.cena_f = Cena(img = FrutosDaTerra)
     self.cena_c = Cena(Calcita, direita=self.cena_f)
-    #self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
+    self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
     #self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
     #esquerda = self.cena_n)
     self.cena_f = self.cena_c
+    self.cena_s.meio = Cena(vai=self.vai_sul)
     #self.cena_n.direita = self.cena_l
     #self.cena_o.esquerda = self.cena_s
     #self.cena_s.esquerda = self.cena_l
@@ -25,9 +26,9 @@ class CenaFrutosDaTerra():
     self.cena_f.vai()
   def vai(self, *_):
     self.cena_f.vai()
-#  def vai_sul(self, *_):
-    #from anastasia.main import CenaAnhanguera
-    #CenaAnhanguera().vai()
+  def vai_sul(self, *_):
+    from anastasia.main import CenaAnhanguera
+    CenaAnhanguera().vai()
 
 def vai_CenaFrutosDaTerra():
   cena1 = CenaFrutosDaTerra()
