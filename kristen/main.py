8142976bdedbde05a50ef32fb8d752c02b8e7326
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:20:30 -0300

    Saved kristen/main.py at Timestamp: 2018-04-18 14:20:30

--- kristen/main.py
+++ kristen/main.py
@@ -3,20 +3,20 @@ from _spy.vitollino.main import Cena, STYLE
 
 STYLE['width'] = 700
 
-cretaceo_s = "https://i.imgur.com/EwPWbms.jpg"
+cretaceo_n = "https://i.imgur.com/EwPWbms.jpg"
 cretaceo_o = "https://i.imgur.com/c9lK8Vm.jpg"
-cretaceo_n = "https://i.imgur.com/QFLlccY.jpg"
+cretaceo_s = "https://i.imgur.com/QFLIccY.jpg"
 cretaceo_l = "https://i.imgur.com/K1LChZw.jpg"
 
 class CenaCretaceo():
   def __init__(self):
+    #from meredith.main import Cena2
     self.cena_n = Cena(img = cretaceo_n)
     self.cena_o = Cena(cretaceo_o, direita=self.cena_n)
     self.cena_s  = Cena(cretaceo_s, direita=self.cena_o)
     self.cena_l  = Cena(cretaceo_l, direita=self.cena_s,
     esquerda = self.cena_n)
     self.cena_n.esquerda = self.cena_o
-    self.cena_n.direita = self.cena_l
     self.cena_o.esquerda = self.cena_s
     self.cena_s.esquerda = self.cena_l
     #cena2 = Cena2()
@@ -25,9 +25,6 @@ class CenaCretaceo():
     self.cena_n.vai()
   def vai(self, *_):
     self.cena_n.vai()
-  def vai_sul(self, *_):
-    from anastasia.main import CenaAnhanguera
-    CenaAnhanguera().vai()
 
 def Game():
   cena1 = CenaCretaceo()
