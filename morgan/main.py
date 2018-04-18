eb80e819a898ae645c1627b68e46ce84cfc58124
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:28:36 -0300

    Saved morgan/main.py at Timestamp: 2018-04-18 14:28:36

--- morgan/main.py
+++ morgan/main.py
@@ -8,15 +8,14 @@ parede2 = "https://i.imgur.com/4Xx7uox.jpg"
 parede3 = "https://i.imgur.com/icYyd5H.jpg"
 
 class Cenatectonica():
- def __init__(self):
-
+    def __init__(self):
     self.parede1 = Cena(img = parede1)
     self.parede2 = Cena(parede2, direita=self.parede1)
-    self.parede3 = Cena(parede3, direita=self.parede2)
+    self.parede3 = Cena(parede3, esquerda=self.parede2)
     
     def Game():
-  cena1 = Cenatectonica()
-  cena1.vai()
+        parede2 = Cenatectonica()
+        parede2.vai()
 
 if __name__ == "__main__":
 	Game()
