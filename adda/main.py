c2e05a6cf0546cec85b0005f9a7224a79e5dd317
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:22:54 -0300

    Saved adda/main.py at Timestamp: 2018-04-18 14:22:54

--- adda/main.py
+++ adda/main.py
@@ -3,9 +3,9 @@ from _spy.vitollino.main import Cena, STYLE
 
 STYLE['width'] = 700
 
-cretaceo_s = "https://drive.google.com/open?id=1HigI3FQMSNJnskLqi30cvTxndG6_CLSE"
+cretaceo_s = "https://i.imgur.com/EwPWbms.jpg"
 cretaceo_o = "https://i.imgur.com/c9lK8Vm.jpg"
-cretaceo_n = "https://i.imgur.com/QFLlccY.jpg"
+cretaceo_n = "https://drive.google.com/open?id=1HigI3FQMSNJnskLqi30cvTxndG6_CLSE"
 cretaceo_l = "https://i.imgur.com/K1LChZw.jpg"
 
 class CenaCretaceo():
