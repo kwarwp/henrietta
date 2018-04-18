a189ec3eb7d8ebc9d4143ea2a4f4d3855d22b44c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-18 11:27:15 -0300

    Saved lisa/main.py at Timestamp: 2018-04-18 14:27:14

--- lisa/main.py
+++ lisa/main.py
@@ -8,12 +8,12 @@ dino_esquerda = "https://i.imgur.com/P54FLEU.jpg"
 
 class CenaDino():
     def __init__(self):
-self.cena_direita = Cena(img = dino_direita)
-self.cena_esquerda = Cena(img = dino_esquerda, direita=self.cena_direita)
-self.cena_direita.esquerda = self.cena_esquerda
-self.cena_direita.direita = self.cena_esquerda
-self.cena_esquerda.direita = self.cena_direita
-self.cena_esquerda.esquerda = self.cena_direita
+      self.cena_direita = Cena(img = dino_direita)
+      self.cena_esquerda = Cena(img = dino_esquerda, direita=self.cena_direita)
+      self.cena_direita.esquerda = self.cena_esquerda
+      self.cena_direita.direita = self.cena_esquerda
+      self.cena_esquerda.direita = self.cena_direita
+      self.cena_esquerda.esquerda = self.cena_direita
     #cena2 = Cena2()
     #self.cena1.direita = cena2
     
