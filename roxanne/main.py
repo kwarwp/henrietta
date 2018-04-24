63c4bf181c66c60d11548ad8a2fd783b723b0557
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-24 11:45:29 -0300

    Saved roxanne/main.py at Timestamp: 2018-04-24 14:45:28

--- roxanne/main.py
+++ roxanne/main.py
@@ -7,42 +7,41 @@ entra_nzoom = "https://i.imgur.com/ELvdLzp.jpg"
 entra_nabre = "https://i.imgur.com/u6ou7mM.jpg"
 entra_l = "https://i.imgur.com/F9Cem7k.jpg"
 entra_o = "https://i.imgur.com/gO8JFgU.jpg"
-entra_ozoom = "https://i.imgur.com/JBQY8wh.jpg"
+entra_oz = "https://i.imgur.com/JBQY8wh.jpg"
 
 
 class Entrada():
    def __init__ (self):
-      
+   
+      self.cena_n  = Cena(img = entra_n) 
       self.cena_na = Cena(img = entra_nabre)
-      self.cena_nz = Cena(entra_nzoom, meio = self.cena_na)
-      self.cena_n = Cena(entra_n, esquerda = self.cena_nz) 
+      self.cena_nz = Cena(img = entra_nzoom)
       
-      self.cena_l = Cena(entra_l, esquerda = self.cena_n)
+      self.cena_l  = Cena(img = entra_l, esquerda = self.cena_n)
       
-      self.cena_oz = Cena(entra_ozoom, direita = self.cena_n)
-      self. cena_o = Cena(entra_o, esquerda = self.cena_oz, 
-      direita = self.cena_n)
+      self. cena_o = Cena(img = entra_o, direita = self.cena_n)
+      self.cena_oz = Cena(img = entra_oz, direita = self.cena_o)
      
       self.cena_n.esquerda = self.cena_o
-      self.cena_o.direita = self.cena_n
+      self.cena_o.esquerda= self.cena_oz
+      self.cena_oz.direita = self.cena_n
       self.cena_n.meio = self.cena_nz
       self.cena_nz.meio = self.cena_na
-      self.cenanz.direita = Cena(vai =self.vai_cret)
+      self.cena_nz.esquerda = Cena(vai = self.vai_sul)
+      
       
       self.cena_n.vai()
-      
+   
    def vai(self, *_):
-    self.cena_n.vai()
+    self.cena_o.vai()
     
-   def vai_cret(self, *_ ):
-    #pode receber tantos argumentos além desse
+   def vai_sul(self, *_):
     from Kristen.main import go_cretaceo
     go_cretaceo()
 
-                       
-def Go_entrada():
-  Entrada = Entrada()
-  Entrada.vai()
+def entrada():
+  museu = Entrada()
+  museu.vai()
 
 if __name__ == "__main__":
-	Go_entrada()
+	entrada()
