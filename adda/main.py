21cdcac3c5ed915731f4c84fe34d31b346df088c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-25 11:01:07 -0300

    Saved adda/main.py at Timestamp: 2018-04-25 14:01:07

--- adda/main.py
+++ adda/main.py
@@ -11,32 +11,33 @@ class CenaTutorialInterativo():
   def __init__(self):
     self.cena_t = Cena(img = TutorialInterativo)
     
+    self.cena_t.esquerda = Cena(vai=self.vai_esquerda)
+    self.cena_t.vai()
     
-
-class CenaEsquerda():
-  def __init__(self):
-    self.cena_e = Cena(img = Cena_esquerda)
-
-class CenaDireita():
-  def __init__(self):
-    self.cena_d = Cena(img = Cena_direita)
-
-
-
+    self.cena_t.meio = Cena(vai=self.vai_importar)
+    self.cena_t.vai()
+    
+    self.cena_t.direita = Cena(vai=self.vai_direita)
+    self.cena_t.vai()
+    
   def vai(self, *_):
     self.cena_t.vai()
+    
+    self.cena_e = Cena(Cena_esquerda, esquerda=self.cena_t)
+    self.cena_t = self.cena_e
+    
+    self.cena_d = Cena(Cena_direita, direita=self.cena_t)
+    self.cena_t = self.cena_d
+    
+  def vai_esquerda(self, *_):
+    vai()
     
   def vai_importar(self, *_):
     from kristen.main import go_cretaceo
     go_cretaceo()
-    
-  def vai_esquerda(self, *_):
-    CenaEsquerda()
-    
+
   def vai_direita(self, *_):
-    CenaDireita()
-    
-    
+    vai()
     
 def vai_CenaTutorialInterativo():
   cenaImporta = CenaTutorialInterativo()
@@ -44,21 +45,3 @@ def vai_CenaTutorialInterativo():
 
 if __name__ == "__main__":
 	vai_CenaTutorialInterativo()
-    
-    
-    
-def vai_CenaEsquerda():
-  cenaEsq = CenaEsquerda()
-  cenaEsq.vai()
-
-if __name__ == "__main__":
-	vai_CenaEsquerda() 
-    
-    
-    
-def vai_CenaDireita():
-  cenaDir = CenaDireita()
-  cenaDir.vai()
-
-if __name__ == "__main__":
-	vai_CenaDireita()     
