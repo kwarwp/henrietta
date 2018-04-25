76b6b5c3497756cdd507e22bb748747b8290daf1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-25 11:07:44 -0300

    Saved adda/main.py at Timestamp: 2018-04-25 14:07:44

--- adda/main.py
+++ adda/main.py
@@ -9,8 +9,7 @@ Cena_esquerda = "https://i.imgur.com/4hYhuEQ.png"
 
 class CenaTutorialInterativo():
   def __init__(self):
-    self.cena_t = Cena(img = TutorialInterativo)
-    self.cena_t.vai()
+    
     
     self.cena_e = Cena(Cena_esquerda, esquerda=self.cena_t)
     self.cena_t = self.cena_e
@@ -20,6 +19,9 @@ class CenaTutorialInterativo():
     
     self.cena_d = Cena(Cena_direita, direita=self.cena_t)
     self.cena_t = self.cena_d
+    
+    self.cena_t = Cena(img = TutorialInterativo)
+    self.cena_t.vai()
 
     
     
