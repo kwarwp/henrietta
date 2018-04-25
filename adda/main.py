5c43a24351846ee88e1fc271e87de12f329c8ad7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-25 11:27:24 -0300

    Saved adda/main.py at Timestamp: 2018-04-25 14:27:23

--- adda/main.py
+++ adda/main.py
@@ -9,13 +9,25 @@ Cena_esquerda = "https://i.imgur.com/4hYhuEQ.png"
 
 class CenaTutorialInterativo():
   def __init__(self):
+    
+    self.cena_i = Cena
+    
+    self.cena_d = Cena(Cena_direita, direita=self.cena_i)
+    
+    
+    self.cena_t  = Cena(TutorialInterativo, direita=self.cena_d,
+    esquerda = self.cena_e)
+
+    
+    self.cena_e  = Cena(Cena_esquerda, esquerda=self.cena_i)
+    
+    
     self.cena_t = Cena(img = TutorialInterativo)
     
-    self.cena_e = Cena(Cena_esquerda, esquerda=self.cena_t)
-    self.cena_t = self.cena_e
-    
-    self.cena_d = Cena(Cena_direita, direita=self.cena_t)
-    self.cena_t = self.cena_d
+    self.cena_i.direita = self.cena_d
+    self.cena_d.direita = self.cena_t
+    self.cena_i.esquerda = self.cena_e
+    self.cena_e.esquerda = self.cena_t
     
     self.cena_t.meio = Cena(vai=self.vai_importar)
     self.cena_t.vai()
