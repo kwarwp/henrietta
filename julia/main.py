3cbf39ce701ca27d8371ebdd3256ccdfa381e8bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-29 19:54:17 -0300

    Saved julia/main.py at Timestamp: 2018-04-29 22:54:17

--- julia/main.py
+++ julia/main.py
@@ -1,56 +1,196 @@
 # henrietta.julia.main.py
-from _spy.vitollino.main import Cena, Texto, Elemento
-from _spy.vitollino.main import INVENTARIO as inv
 
-from _spy.vitollino.main import Cena, Texto, Elemento
-from _spy.vitollino.main import INVENTARIO as inv
+from browser import document, alert, html
+from _spy.vitollino.main import Cena, STYLE
+STYLE['width'] = 740
+#STYLE['height'] = "100%"
 
-TEMPLO = "http://3.bp.blogspot.com/-UsnGAupu3XM/VHvU2M5BHUI/AAAAAAAAdCE/UKbq_5dTM7k/s1600/IMG_6098.JPG"
-CORREDOR = "http://i.muyinteresante.com.mx/dam/sociedad/historia/17/03/8/rabbit-hole-700-year-old-secret-knights-templar-cave-network-8-58c006f4a30df__880.jpg.imgo.jpg"
+A_NORTE = "https://i.imgur.com/aLEjWgB.png"
+
+def _main():
+    document['pydiv'].html = ""
+    a_norte = Cena(img=A_NORTE)
+    a_norte.vai()
+    
+# main()
+m6n="https://i.imgur.com/oThg1nq.jpg"
+m6n="https://i.imgur.com/oThg1nq.jpg"
+g6e="https://i.imgur.com/4vjrEEG.jpg"
+g6n="https://i.imgur.com/DcyUIgN.jpg"
+g6s="https://i.imgur.com/TbdLoXs.jpg"
+wpt="https://i.imgur.com/vc9RMEN.png"
+oce = "https://i.imgur.com/oDqeaBp.jpg"
 OCEANO = "https://freeclipartspot.com//storage/upload/ocean-clip-art/ocean-clip-art-51.jpg"
-ALGA = "https://i.pinimg.com/originals/70/68/5f/70685fa634c3bb82a8eb5771a0a869ed.png"
-CONCHA = "http://www.mat.uc.pt/~picado/conchas/imagens/p10.png"
-AQUARIO = "https://www.tenstickers.pt/autocolantes-decorativos/img/preview/autocolante-decorativo-infantil-peixe-aquario-3634.png"
-FLORESTA = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW0BY3YH52d4HOogtzl4XDXyawkVJeAi-5pNTHvhQZBrB-OF2i"
-CORUJA = "http://www.eternit.com.br/images/files_up/original/29042015150454corujacomasasfechadasnopuleiro_emalta.jpg"
-PATOS = "http://4.bp.blogspot.com/-aF5frApwsMs/UnCGJk14WEI/AAAAAAAAahs/FKS72dTtYzk/s1600/Animais-em-png-queroimagem-Cei%C3%A7a-Crispim++(12).png"
-MACACO = "https://i.pinimg.com/originals/2e/75/f6/2e75f60ab1931937f8fadcca38630382.png"
-FLORES = "https://banner.kisspng.com/20171127/1a3/yellow-rose-bush-png-clipart-picture-5a1bab2ea27e19.9392345015117627346656.jpg"
-PASSAROS = "https://png.clipart.me/previews/272/cute-twitter-bird-vector-image-26050.png"
-MUSICA = "https://images.vexels.com/media/users/3/148533/list/ab32e2a639adb5969b49b6420daf0514-boys-band-cartoon-illustration.jpg"
-HEADPHONES = "https://images.vexels.com/media/users/3/144117/isolated/preview/b37cdf9a9b5ebc1554ffe1a7825939fc-fones-de-ouvido-de-msica-by-vexels.png"
-VIOLAO = "http://cdn5.colorir.com/desenhos/color/201101/024b7852c59f132874fea13c36d4887f_163.png"
-CHOCALHO = "https://i.pinimg.com/originals/92/fc/c2/92fcc2e8c29c17242612acef1e457bea.png"
-TAMBOR = "https://images.emojiterra.com/emojione/v2/512px/1f941.png"
-MUSEU = "https://cdna.artstation.com/p/assets/images/images/004/585/984/large/kamila-redkiewicz-museum.jpg?1484770586"
-COROA = "https://medologia.com/assets/images/huol9FtnOB5KjNVjjM04keqhmLYKwIeyd3uEUluBXoPMxBp9J0Wex3CFqBrZt0SL4NPSV9qrNIblOg7pOwjUI6YDqr0SqT4M4Y2n1eU0KWz9saMKCGDYYf8I.png"
-VASO = "http://www.fundaj.gov.br/images/stories/museu/museu3.png"
-RELOGIO = "http://presidenteprudente.sp.gov.br/museu/images/2028927.png"
-LUNETA = "http://www.pngpix.com/wp-content/uploads/2016/07/PNGPIX-COM-Spyglass-Telescope-PNG-Transparent-Image.png"
-COZINHA = "https://image.freepik.com/vettori-gratuito/interno-cucina-moderna-piatto_6280-115.jpg "
-COZINHEIRA = "http://ead.ceiprojac.com.br/Assets/Categorias/M_60371d747e33a01.png"
-CADEIRA = "http://www.reformatapecaria.com.br/public/img/default/servicos-de-tapecaria/home/reforma-cadeiras-de-cozinha.png"
-PANELA = "https://tramontina.com.br/public/panelas-inox/solar/img/produto.png?v=7"
-FOGAO = "http://clipart.coolclips.com/480/vectors/tf05085/CoolClips_hous1104.png"
-ESPORTE = "https://img3.stockfresh.com/files/d/ddraw/m/72/2405156_stock-photo-sport-characters-with-background.jpg"
-BOLA = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Soccer_ball.svg/800px-Soccer_ball.svg.png"
-RAQUETE = "http://padelcristalia.com/public/uploads/padelcristalia/logopala.png"
-LUVAS = "https://static.vecteezy.com/system/resources/previews/000/143/644/non_2x/boxing-gloves-vector.png"
-SALATEMPLO = "https://i.imgur.com/kmLevtL.jpg"
-CINZA = "https://cdn.simplo7.net/static/7106/sku/tricoline-lisa-100-algodao-tricoline-lisa-100-algodao-cinza-1921-50cm-x-1-50mt--p-1502305300683.jpg"
+from _spy.vitollino.main import Cena, Elemento, Droppable, Dragger
 
 
-class Cenatemplo():
- 	def __init__(self):
-    	self.templo = Cena(img=TEMPLO)
-    	self.corredor = Cena(img=CORREDOR)
-    	templo.centro = corredor
-   	 
-    	oceano = Elemento(img=OCEANO,tit="oceano",style=dict(left=100, top=160, width=60, height=200))
-    	oceano.entra(corredor)
-   	 
-    	
+class Planta(Cena, Droppable):
 
-CORREDOR = "http://i.muyinteresante.com.mx/dam/sociedad/historia/17/03/8/rabbit-hole-700-year-old-secret-knights-templar-cave-network-8-58c006f4a30df__880.jpg.imgo.jpg”
+    def __init__(self, *a, **k):
+        Cena.__init__(self, *a, **k)
+        Droppable.__init__(self, self.divesq, "regador", self.regou)
 
-PORTA = "https://formentinatura.files.wordpress.com/2013/11/templo-del-sol-entrada-original-conjunto-2-machu-picchu-cuzco-perc3ba-c2a9-formentc3ad-031.jpg?w=497”
+    def regou(self, *_):
+        alert("VocÃƒÂª regou a planta")
+        
+class Regador(Elemento, Dragger):
+
+    def __init__(self, *a, **k):
+        Elemento.__init__(self, *a, **k)
+        Dragger.__init__(self, self.img)
+
+
+def main():
+    cenae = Cena(g6e)
+    cenas = Cena(g6s)
+    cenan = Planta(g6n, direita=cenae)
+    cenae.direita = cenas
+    cenas.esquerda = cenae
+    cenae.esquerda = cenan
+    cenan.esquerda = cenas
+    cenas.direita = cenan
+    #print(dir(cenas))
+    cenan.vai()
+    rega = Regador(wpt, style=dict(width=60, height=60, left=200, top=200), tit="regador")
+    rega.entra(cenan)
+
+
+
+class Folha:
+    def __init__(self, bloco, left=0, top=0, ileft=0, itop=0,
+                 size=dict(width="100px", height="100px")):
+        self.suporte = None
+        w, h = int(size['width'][:-2]), int(size['height'][:-2])
+        ileft, itop = "%dpx" % (ileft*w), "%dpx" % (itop*h)
+        style = {'position': 'absolute', 'overflow': 'hidden', 'margin':'1%',
+                'background-image': 'url({})'.format(bloco.img),
+                'background-position': '{} {}'.format(ileft, itop),
+                'background-size': '{}px {}px'.format(400, 400),
+        }
+        image_style = {'position': "relative", 'min-width': '400px',
+        'height': '400px'}  # , 'pointer-events': 'none'}
+        style.update(size)
+        style.update(left="%dpx" % (left*(w+10)), top="%dpx" % (top*(h+10)))
+        #image_style.update(left="%dpx" % (-ileft*w), top="%dpx" % (-itop*h))
+        fid = "folha%d" % (10*top+left)
+        self.folha = html.DIV(Id=fid, style=style, draggable=True)        
+        bloco.folha <= self.folha
+        self.folha.ondragstart = self.drag_start
+        self.folha.onmouseover = self.mouse_over
+        bloco.folhas[fid]=self
+        #self.fo_img.ondragstart = self.img_drag_start
+
+    def mouse_over(self, ev):
+        ev.target.style.cursor = "pointer"
+        return False
+
+    def img_drag_start(self, ev):
+        ev.preventDefault()
+        ev.stopPropagation()
+        return False
+
+    def drag_start(self, ev):
+        ev.stopPropagation()
+        ev.data['text'] = ev.target.id
+        ev.data.effectAllowed = 'move'
+        return False
+
+    def troca(self, suporte):
+        self.folha.style.left = 0
+        self.folha.style.top = 0
+        suporte.recebe(self, self.suporte)
+        self.suporte = suporte
+        self.folha.style.cursor = "auto"
+
+
+class Suporte:
+    def __init__(self, bloco, certa, left=0, top=0,
+                 size=dict(width="25%", height="25%")):
+        self.ladrilho = None
+        style = {'position': "absolute", 'overflow': 'hidden',
+                 'border':'1px solid white'}
+        w, h = int(size['width'][:-1]), int(size['height'][:-1])
+        style.update(size)
+        style.update(left="%d%%" % (left*w), top="%d%%" % (top*h))
+        self.certa = certa
+        self.folha = html.DIV(style=style)
+        bloco.suporte <= self.folha
+        self.folha.ondragover = self.drag_over
+        self.folha.ondrop = self.drop
+        self.bloco = bloco
+
+    def recebe(self, folha, suporte):
+        self.folha <= folha.folha
+        suporte.recebe(self.ladrilho, None) if suporte else None
+        self.ladrilho = folha
+
+    def drag_over(self, ev):
+        ev.data.dropEffect = 'move'
+        ev.preventDefault()
+        return False
+
+    def drop(self, ev):
+        ev.preventDefault()
+        ev.stopPropagation()
+        src_id = ev.data['text']
+        self.bloco.folhas[src_id].troca(self) 
+        """
+        certa = True
+        if src_id != self.certa:
+            elt.style.background = "red"
+            certa = False
+            self.bloco.conta_pecas(certa)
+        """
+        #return False
+
+
+class Bloco:
+    def __init__(self, img):
+        self.img = img
+        self.folhas = {}
+        self.monta = lambda *_: None
+        ordem = ["%02d"%x for x in range(16)]
+        desordem = ordem[:]
+        from random import shuffle
+        shuffle(desordem)
+        self.tela = document["pydiv"]
+        self.suporte = html.DIV(style=dict(position="absolute",
+        left=10, top=20, width=400, height='%dpx'%400, border=1,
+        borderColor="white"))
+        self.folha = html.DIV(style=dict(position="absolute",
+        left=410, top=20, width=450, height='%dpx'%450))
+        self.tela.html = ""
+        self.tela <= self.suporte
+        self.tela <= self.folha
+        self.pecas_colocadas = []
+        #print(list(enumerate(ordem)))
+        for pos, fl in enumerate(ordem):
+            Suporte(self, "folha" + fl, pos//4, pos%4)
+        for pos, tx in enumerate(desordem):
+            Folha(self, pos//4, pos%4, int(tx)//4, int(tx)%4)
+
+    def inicia_de_novo(self):
+        pass
+
+    def conta_pecas(self, valor_peca):
+        self.pecas_colocadas += valor_peca
+        if len(self.pecas_colocadas) == 4:
+            if all(self.pecas_colocadas):
+                input("O texto esta certo.")
+            else:
+                vai = input("Tentar de novo?")
+                if vai == "s":
+                    self.inicia_de_novo()
+
+    def nao_monta(self):
+        pass
+
+    def vai(self):
+        self.monta()
+        self.monta = self.nao_monta
+        # self.centro.norte.vai()
+
+
+if __name__ == "__main__":
+    #main()
+    Bloco(OCEANO)
