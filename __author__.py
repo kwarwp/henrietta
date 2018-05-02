35de6690ecdb09546aa28a4bd47eee9a3d31ecb3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 01:11:45 -0300

    added faulty soraya

--- __author__.py
+++ __author__.py
@@ -1,2 +1,3 @@
 {
 "henrietta": {"author_nick": "fono-games", "author_name": "Games Inteligentes e Fonoaudiologia Computacional", "author_email": "labasence@gmail.com", "author_org": "LABASE-FONOCOMP", "author_site": "http://www.superpython.net", "author_public": "on"},
+"soraya": {"author_nick": "carlo", "author_name": "Carlo Oliveira", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "", "author_public": "on"},
