143905cfde365f6621102943c892b8c701bb0788
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 00:57:06 -0300

    Created __author__.py at Timestamp: 2018-05-02 00:57:05

---
+++ __author__.py
@@ -0,0 +1,1 @@
+"henrietta": {"author_nick": "fono-games", "author_name": "Games Inteligentes e Fonoaudiologia Computacional", "author_email": "labasence@gmail.com", "author_org": "LABASE-FONOCOMP", "author_site": "http://www.superpython.net", "author_public": "on"},
