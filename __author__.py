63504d79d3ea9e3e630045a0320ae2fe939da806
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 01:09:57 -0300

    fix fo json henrietta

--- __author__.py
+++ __author__.py
@@ -1,1 +1,2 @@
+{
 "henrietta": {"author_nick": "fono-games", "author_name": "Games Inteligentes e Fonoaudiologia Computacional", "author_email": "labasence@gmail.com", "author_org": "LABASE-FONOCOMP", "author_site": "http://www.superpython.net", "author_public": "on"},
