46edbd5f52a43832fa3a3bb9bd6d033dacea2b18
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-01 23:27:16 -0300

    Saved kellee/main.py at Timestamp: 2018-05-02 02:27:16

--- kellee/main.py
+++ kellee/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 NEWTON = "https://netnature.files.wordpress.com/2017/11/sem-tc3adtulo37.png?w=524&h=324"
 MACA = "https://amdpc.files.wordpress.com/2008/03/apple_computer_logo_svg.png"
 GALILEU = "https://pbs.twimg.com/profile_images/666413287639269377/-Yw4RtjG_400x400.png"
-ORBITA = "https://upload.wikimedia.org/wikipedia/commons/4/48/Sobo_1909_768.png"
+ORBITA = "https://upload.wikimedia.org/wikipedia/commons/a/a9/Illuminati_triangle_eye.png"
 
 
 
@@ -12,7 +12,7 @@ def teorias():
 
     inicio = Cena(img= NEWTON)
     inicio_mud = Elemento(img = MACA, tit="Eis que a gravidade atua", style = dict(right= 100,
-                                                                                   top=200,
+                                                                                   top=40,
                                                                                    width=110,bottom=20))
     lado1 = Cena(img = GALILEU, direita = inicio)
     lado1_mud = Elemento(img = ORBITA, tit = "O mundo não gira em torno de você não,@", style = dict(left = 100,
