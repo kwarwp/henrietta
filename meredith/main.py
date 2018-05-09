793beeb2e6a8a7c4bfda36135ebfbd91f40aa617
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-09 16:54:08 -0300

    Saved meredith/main.py at Timestamp: 2018-05-09 19:54:08

--- meredith/main.py
+++ meredith/main.py
@@ -1,1 +1,21 @@
 # henrietta.meredith.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import STYLE
+STYLE ["Width"] = 10000
+
+
+ARVORE = "https://static.significados.com.br/foto/arvore.jpg"
+MAE = 'https://media.istockphoto.com/vectors/cartoon-mother-holding-her-son-vector-id506469526'
+
+def familia ():
+    arvore = Cena(img = ARVORE)
+    arvore.vai()
+    mae = Elemento(img = MAE, tit = "Mother", style = dict(left=50, top=200, width=50, high=50))
+    mae_text= Texto(arvore, "Mother")
+    mae.vai = mae_text.vai
+    mae.entra(arvore)
+    
+familia ()
+    
+
+
