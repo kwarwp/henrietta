8c5ca75fc82a1db1ed8b122cc80a765fe7a65f49
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-10 17:24:49 -0300

    Saved natalia/main.py at Timestamp: 2018-05-10 20:24:49

--- natalia/main.py
+++ natalia/main.py
@@ -1,5 +1,5 @@
 # henrietta.natalia.main.py
-verbos_altos = ["ger", "atrit", "roç", "direcion", "friccion", "elev", "decid", "faz", "concl", "us",
+verbos_altos = ["ger", "atrit", "roÃ§", "direcion", "friccion", "elev", "decid", "faz", "concl", "us",
                 "remanej" ,"erg", "suspend", "ate", "esfreg", "trisc"] ==  3
 verbos_medios = ["bat", "gir", "coloc", "manipul", "mov", "surg", "peg", "levant", "bat"] ==  2
 verbos_fracos = ["rod", "bot", "sub", "pux", "form", "tent", "clic", "abaix", "mex", "encost", "rel"] ==  1
