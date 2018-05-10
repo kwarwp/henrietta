483f52b72151c64b4afa4befe47c70033b76d13e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-10 12:10:56 -0300

    Saved henrietta/__log__.py at Timestamp: 2018-05-10 12:09:38

--- henrietta/__log__.py
+++ henrietta/__log__.py
@@ -45,4 +45,27 @@ XXXX - executa_acao if error else lambda self.error - XXXX
   move="BLOCO", ponto=self.repete, valor=0)
                                           ^
 SyntaxError: invalid syntax
-XXXX - executa_acao if error else lambda self.error - XXXX
+XXXX - executa_acao if error else lambda self.error - XXXXTraceback (most recent call last):
+  module _core.main line 204
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 132
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 286
+    return self._first_response(dialog, lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 261
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 278
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 167
+    Puzzle()
+  module <module> line 159
+    self.puzzle = Bloco(oce, 4, 4, style=dict(left=10, top=100))
+  module <module> line 116
+    self.inicia_de_novo()
+  module <module> line 138
+    Folha(self, pos%nx, pos//nx, int(tx)%nx, int(tx)//nx,
+  module <module> line 30
+    INVENTARIO.score(casa=self.casa, carta=self.fid, move="INIT", ponto=0, valor=0)
+  module _spy.vitollino.main line 369
+    SUPERPYTHON.scorer(data) if SUPYGIRLS else None
+NameError: name 'SUPYGIRLS' is not defined
