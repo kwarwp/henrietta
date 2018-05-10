52efce1d9d44e5cd3d2f44a80c66a417c9ed43b0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-10 18:16:16 -0300

    Saved natalia/main.py at Timestamp: 2018-05-10 21:16:16

--- natalia/main.py
+++ natalia/main.py
@@ -1,21 +1,21 @@
 # henrietta.natalia.main.py
 verbos_altos = ["ger", "atrit", "roÃÂÃÂ§", "direcion", "friccion", "elev", "decid", "faz", "concl", "us",
                 "remanej" ,"erg", "suspend", "ate", "esfreg", "trisc"] 
-#verbos_altos ==  3
+verbos_altos ==  3
 verbos_medios = ["bat", "gir", "coloc", "manipul", "mov", "surg", "peg", "levant", "bat"]
-#==  2
+verbos_medios ==  2
 
 verbos_fracos = ["rod", "bot", "sub", "pux", "form", "tent", "clic", "abaix", "mex", "encost", "rel"] 
-#==  1
+verbos_fracos ==  1
 
-verbos = [123, verbos_altos,verbos_medios,verbos_fracos]
+verbos = [321,(3, verbos_altos),(2,verbos_medios),(1,verbos_fracos)]
 
 def avaliar(you):
     for they in you:
         if isinstance(they,list):
             print("da uma elevada:")
         else:
-            return False
+             print(False)
     
 avaliar(verbos)
     
