a82477e449d661c33b7b48327ba302df2b422e3a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-10 18:38:12 -0300

    Saved natalia/main.py at Timestamp: 2018-05-10 21:38:12

--- natalia/main.py
+++ natalia/main.py
@@ -1,4 +1,5 @@
 # henrietta.natalia.main.py
+from operator import eq
 verbos_altos = ["ger", "atrit", "roÃÂÃÂ§", "direcion", "friccion", "elev", "decid", "faz", "concl", "us",
                 "remanej" ,"erg", "suspend", "ate", "esfreg", "trisc"] 
 verbos_altos ==  3
@@ -7,13 +8,12 @@ verbos_medios ==  2
 
 verbos_fracos = ["rod", "bot", "sub", "pux", "form", "tent", "clic", "abaix", "mex", "encost", "rel"] 
 verbos_fracos ==  1
-
-verbos = int[321,(3, verbos_altos),(2,verbos_medios),(1,verbos_fracos)]
+verbos = [321,eq (3, verbos_altos),eq(2,verbos_medios),eq(1,verbos_fracos)]
 
 def avaliar(you):
     for they in you:
         if isinstance(they,int):
-            print("da uma elevada:")
+            print("da uma elevada:", you.index("{}"))
         else:
              print(False)
     
