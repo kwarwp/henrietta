2e69d84652fda3cdfb1e849a04c05a69c286e735
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-10 12:07:23 -0300

    Saved henrietta/__log__.py at Timestamp: 2018-05-10 12:06:05

--- henrietta/__log__.py
+++ henrietta/__log__.py
@@ -18,4 +18,25 @@ XXXX - executa_acao if error else lambda self.error - XXXXXXXX - executa_acao if
   module <module> line 119
     INVENTARIO.score(casa=self.img, carta=self.dim[0]*100+self.dim[1],
 NameError: name 'INVENTARIO' is not defined
-XXXX - executa_acao if error else lambda self.error - XXXX
+XXXX - executa_acao if error else lambda self.error - XXXXTraceback (most recent call last):
+  module _core.main line 204
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 132
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 286
+    return self._first_response(dialog, lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 261
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 278
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 167
+    Puzzle()
+  module <module> line 159
+    self.puzzle = Bloco(oce, 4, 4, style=dict(left=10, top=100))
+  module <module> line 116
+    self.inicia_de_novo()
+  module <module> line 119
+    INVENTARIO.score(casa=self.img, carta=self.dim[0]*100+self.dim[1],
+  module _spy.vitollino.main line 369
+    SUPERPYTHON.scorer(data) if SUPYGIRLS else None
+NameError: name 'SUPYGIRLS' is not defined
