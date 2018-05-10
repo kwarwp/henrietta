c5e9a979c709a7a65d06f74ead30edbaba18fe67
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-10 17:48:36 -0300

    Saved natalia/main.py at Timestamp: 2018-05-10 20:48:36

--- natalia/main.py
+++ natalia/main.py
@@ -1,14 +1,22 @@
 # henrietta.natalia.main.py
 verbos_altos = ["ger", "atrit", "roÃ§", "direcion", "friccion", "elev", "decid", "faz", "concl", "us",
-                "remanej" ,"erg", "suspend", "ate", "esfreg", "trisc"] ==  3
-verbos_medios = ["bat", "gir", "coloc", "manipul", "mov", "surg", "peg", "levant", "bat"] ==  2
-verbos_fracos = ["rod", "bot", "sub", "pux", "form", "tent", "clic", "abaix", "mex", "encost", "rel"] ==  1
-verbos = [321,verbos_altos,verbos_medios,verbos_fracos]
+                "remanej" ,"erg", "suspend", "ate", "esfreg", "trisc"] 
+#verbos_altos ==  3
+verbos_medios = ["bat", "gir", "coloc", "manipul", "mov", "surg", "peg", "levant", "bat"]
+#==  2
+
+verbos_fracos = ["rod", "bot", "sub", "pux", "form", "tent", "clic", "abaix", "mex", "encost", "rel"] 
+#==  1
+
+verbos = [123, verbos_altos,verbos_medios,verbos_fracos]
 
 def avaliar(list):
-    
     for x in verbos:
-        print("da uma olhada", verbos.index(verbos_altos))
+        if isinstance(verbos,list):
+            for z in verbos : 
+                print("da uma elevada:", verbos.index(verbos))
+        else:
+            print (False)
     
 avaliar(verbos)
     
