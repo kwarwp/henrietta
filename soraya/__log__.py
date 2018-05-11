6d5f0cb7fc9de0c6b3d06c9480143e492c039c6e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-11 08:59:21 -0300

    Saved soraya/__log__.py at Timestamp: 2018-05-11 11:59:21

---
+++ soraya/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri May 11 2018 08:59:21.60 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 152
+  INVENTARIO.score(
+                                                                                                        ^
+SyntaxError: invalid syntax
+'''},
