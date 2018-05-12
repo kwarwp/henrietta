10d99ebf81528833d2680d84b3ad834bda8bf99f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-12 20:31:07 -0300

    Saved soraya/__score__.py at Timestamp: 2018-05-12 20:31:05

--- soraya/__score__.py
+++ soraya/__score__.py
@@ -27,3 +27,4 @@
     {'doc_id': '6663145e', 'carta': 'folha_0_0', 'casa': (1, 0), 'move': 'DROP', 'ponto': 150, 'valor': 'casa_0_0', '_level': 2, 'tempo': 1526167841063},
     {'doc_id': '6663145e', 'carta': 'folha_0_1', 'casa': (1, 1), 'move': 'DROP', 'ponto': 150, 'valor': 'casa_1_0', '_level': 2, 'tempo': 1526167843321},
     {'doc_id': '6663145e', 'carta': 'folha_1_1', 'casa': (0, 0), 'move': 'DROP', 'ponto': 100, 'valor': 'casa_0_1', '_level': 2, 'tempo': 1526167846536},
+    {'doc_id': '6663145e', 'carta': 'folha_1_0', 'casa': (0, 1), 'move': 'DROP', 'ponto': 100, 'valor': 'casa_1_1', '_level': 2, 'tempo': 1526167864942},
