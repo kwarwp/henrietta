51461bdb9ca3c6d8726835718dc66cd65c973929
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-16 09:45:17 -0300

    Created samantha/main.py at Timestamp: 2018-05-16 12:45:17

---
+++ samantha/main.py
@@ -0,0 +1,36 @@
+Historia = "Eu peguei o oculos"
+
+sujeito = ["eu"]
+verbo = ["pegu","cort","bat"]
+objeto = ["o oculos","a pedr","o galho"]
+
+
+"""estrutura1 = (3,[sujeito, verbo,objeto])
+estrutura2 = (2,[sujeito,objeto,verbo])
+estrutura3 = (2,[objeto, sujeito, verbo])
+estrutura4 = (1,[verbo, sujeito, objeto])
+"""
+
+estrutura1 = [sujeito, verbo,objeto]
+estrutura2 = [sujeito,objeto,verbo]
+estrutura3 = [objeto, sujeito, verbo]
+estrutura4 = [verbo, sujeito, objeto]
+
+estruturax = [(3,estrutura1), (2,estrutura2), (2,estrutura3), (1,estrutura4)]
+
+
+def valora(arq):
+    for estrutura in estruturax:
+        for sentenca in estrutura:
+            if sujeito[0], verbo[1], objeto[2]
+                print(3)
+            elif sujeito[0], objeto[1], verbo[2]
+                print(2)
+                
+valora(historia)
+    
+     
+
+#http://folivetti.github.io/courses/Lcon2016/LCoN2016-RepresentacaoTextual.pdf
+#questão crescente ou descrescente
+# List 
