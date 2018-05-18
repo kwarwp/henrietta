d595bb5a45d96f8b4f5c821bbd0e819de3e8515d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-18 16:20:53 -0300

    Saved alexa/__score__.py at Timestamp: 2018-05-18 16:20:51

--- alexa/__score__.py
+++ alexa/__score__.py
@@ -2,3 +2,5 @@
 "a57555ad": {"author_nick": "carlo", "author_name": "carlo", "author_email": "hello@carlo", "author_org": "carlo", "author_site": "carlo", "author_public": "on"}[,
 ]
 "a7295e31": {"author_nick": "carlo", "author_name": "carlo", "author_email": "hello@carlo", "author_org": "carlo", "author_site": "carlo", "author_public": "on"},[
+]
+"2684f792": {"author_nick": "carlo", "author_name": "carlo", "author_email": "hello@carlo", "author_org": "carlo", "author_site": "carlo", "author_public": "on"},[
