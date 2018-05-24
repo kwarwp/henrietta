4c59133ddce63c8f34d6db95f075b3bad3f796cf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-24 18:21:41 -0300

    Saved heather/main.py at Timestamp: 2018-05-24 21:21:41

--- heather/main.py
+++ heather/main.py
@@ -1,44 +1,51 @@
-
 """ Este algoritmo tem por intuito valorar as sentenÃ§as usuÃ¡rios da FonoComp Game"""
 
 SUJEITO = ["eu"]
-VERBO   = ["peg", "Tris","colh","rel","bat"]
-OBJETO  = ["o oculos", "a pedr", "cascalhos", "galho", "arvore", "lupa", "raios solares"]
+VERBO = ["peg", "Tris", "colh", "rel", "bat"]
+OBJETO = ["o oculos", "a pedr", "cascalhos", "galho", "arvore", "lupa", "raios solares"]
 
 """ As variÃ¡veis: SUJEITO, VERBOS e OBJETOS remetem aos elementos que se esperam na construÃ§Ã£o das 
   sentenÃ§as no game. De inÃ­cio tomamos por relevante frases simples (sujeito + verbo + objeto)."""
-  
+
 ESTRUTURA1 = [SUJEITO + VERBO + OBJETO]
-ESTRUTURA2 = [VERBO +SUJEITO + OBJETOS]
-ESTRUTURA3 = [VERBO + OBJETOS+ SUJEITO]
+ESTRUTURA2 = [VERBO + SUJEITO + OBJETO]
+ESTRUTURA3 = [VERBO + OBJETO + SUJEITO]
 ESTRUTURA4 = [SUJEITO + OBJETO + VERBO]
 ESTRUTURA5 = [OBJETO + SUJEITO + VERBO]
 ESTRUTURA6 = [OBJETO + VERBO + SUJEITO]
-IMPLICITO  = [VERBO + OBJETO]
+IMPLICITO = [VERBO + OBJETO]
+# estes aqui ajudam
+ES1 = "SVO"
+ES2 = "VSO"
+ES3 = "VOS"
+ES4 = "SOV"
+ES5 = "OSV"
+ES6 = "OVS"
+IMPLICITO = [VERBO + OBJETO]
 
-    """ As variÃ¡veis: ESTRUTURAX referem-se Ã s sentenÃ§as alvo, ou seja, as que deseja coletar.
-         ESTRUTURA1 = Eu peguei o oculos. 3pts
-         ESTRUTURA2 = Peguei eu o oculos. 2pts
-         ESTRUTURA3 = Peguei o oculos eu. 1pts
-         ESTRUTURA4 = Eu o oculos peguei. 1pts
-         ESTRUTURA5 = O oculos eu peguei. 2pts
-         ESTRUTURA6 = O oculos peguei eu. 1pts
-         IMPLICITO  = Peguei o oculos.    3pt
-    """
-    
-ESTRUTURAX=dict(E1 = "\U+0053" + "\U+0056" + "\U+004F",
-             E2 = "\U+0056" + "\U+0053" + "\U+004F",
-             E3 = "\U+0056" + "\U+004F" + "\U+0053",
-             E4 = "\U+0053" + "\U+004F" + "\U+0056",
-             E5 = "\U+004F" + "\U+0053" + "\U+0056",
-             E5 = "\U+004F" + "\U+0056" + "\U+0053"
-             IM = "\U+0056" + "\U+004F")
-             
-             #S = U + 0053
-             #V = U+0056
-             #O = U+
-             
-             
+""" As variÃ¡veis: ESTRUTURAX referem-se Ã s sentenÃ§as alvo, ou seja, as que deseja coletar.
+     ESTRUTURA1 = Eu peguei o oculos. 3pts
+     ESTRUTURA2 = Peguei eu o oculos. 2pts
+     ESTRUTURA3 = Peguei o oculos eu. 1pts
+     ESTRUTURA4 = Eu o oculos peguei. 1pts
+     ESTRUTURA5 = O oculos eu peguei. 2pts
+     ESTRUTURA6 = O oculos peguei eu. 1pts
+     IMPLICITO  = Peguei o oculos.    3pt
+"""
+
+ESTRUTURAX = dict(E1="\u0053" + "\u0056" + "\u004F",
+                  E2="\u0056" + "\u0053" + "\u004F",
+                  E3="\u0056" + "\u004F" + "\u0053",
+                  E4="\u0053" + "\u004F" + "\u0056",
+                  E5="\u004F" + "\u0053" + "\u0056",
+                  E6="\u004F" + "\u0056" + "\u0053",
+                  IM = "\u0056" + "\u004F")
+
+# S = U + 0053
+# V = U+0056
+# O = U+
+
+
 """
 SUJEITO = ["eu", "fui", "jornal"]
 
@@ -47,54 +54,61 @@ historia = "eu  fui  ao  barco ontem"
 
 
 def responde(item, tree):
-     """#FunÃ§Ã£o responsÃ¡vel por encontrar a estrutura, parear com a estrutura alvo, apontar o elemento nÃ£o 
-     #encontrado e valorar a sentenÃ§a.
-       
-       #Type: param: object(item: list; tree: string)
-       """
-       
-     
-     
-    branch = tree.split()
-    for amora in item:
-        if amora in branch:
-            print("eba")
- 
-        else:
-            print("not found:" + amora)
+     """  # FunÃ§Ã£o responsÃ¡vel por encontrar a estrutura, parear com a estrutura alvo, apontar o elemento nÃ£o 
+# encontrado e valorar a sentenÃ§a.
+
+# Type: param: object(item: list; tree: string)
+"""
+
+
+
+branch = tree.split()
+for amora in item:
+ if amora in branch:
+     print("eba")
+
+ else:
+     print("not found:" + amora)
 responde(SUJEITO, historia)
 
 
 """
-            
-            
-            
-            
-            
-            
-            
-            
-
 
 SUJEITO = {"eu", "jornal", "elefante"}
-VERBO = {"fui", "pesquei", "colhi", "fiz","peguei","pegar"}
+VERBO = {"fui", "pesquei", "colhi", "fiz", "peguei", "pegar"}
 OBJETO = {"oculos", "galho", "lupa"}
+TAGGER = {tag: clazz for tag, clazz in zip(list("SVO"), (SUJEITO, VERBO, OBJETO))}
+PONTUA = "!,.?:"
 
 historia = "eu  fui  ao  barco ontem! pesquei dois peixes e depois de pegar um galho fiz fogo."
 
 
-
 def responde(item, item2, item3, tree):
+    # quebra em frases, toda a pontucao vira quebr de linha
+    def remarcador_recursivo(_tree, pontua):
+        _tree = _tree.replace(pontua.pop(), '\n')
+        return remarcador_recursivo(_tree, pontua) if pontua else _tree
 
-    branch = tree.split()
+    tree = remarcador_recursivo(tree, list(PONTUA))
+    print("remarcador_recursivo:", tree)
+    # agora que todos os pontos viraram quebra de pagina (\n), podemos quebrar em frases
+    # aproveitamos para quebrar cada frase em palavras
+    tree_with_branches_and_berries = twbb = [branch.split() for branch in tree.split('\n')]
+    tegged = []
+    anytag = set(SUJEITO) | set(VERBO) | set(OBJETO)
+    # vamos colocar as tags nas classes gramaticais
+    tree_with_branches_and_tagged_berries = [
+        [(tag, amora) for amora in branch for tag, samples in TAGGER.items() if amora in samples]
+        for branch in tree_with_branches_and_berries]
+    print("responde marcador:", tree_with_branches_and_tagged_berries)
 
-    for amora in branch:
-         if set(SUJEITO)| set(VERBO) | set(OBJETO)  in branch:
+    """    for amora in branch:
+         if amora   in branch:
              print("yuha")
          else:
              print("putz")
 
-    """for amora in item:
+for amora in item:
         if amora in branch:
             print("eba:" + amora )
 
@@ -116,8 +130,9 @@ def responde(item, item2, item3, tree):
         else:
             print("not found:" + amora)"""
 
+
 responde(SUJEITO, VERBO, OBJETO, historia)
-   
+
     
     
     
