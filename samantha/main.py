c8ef5ef214aa625d013e9e4593f953e4870a1b1c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-24 15:24:46 -0300

    Saved samantha/main.py at Timestamp: 2018-05-24 18:24:46

--- samantha/main.py
+++ samantha/main.py
@@ -1,40 +1,42 @@
-Historia = "Eu peguei o oculos"
-d = historia.split()
 
-sujeito = ["eu"]
-verbo = ["pegu","cort","bat"]
-objeto = ["o oculos","a pedr","o galho"]
+SUJEITO = ["eu", "jornal", "elefante"]
+VERBO = ["fui", "pesquei", "colhi", "fiz","peguei","pegar"]
+OBJETO = ["oculos", "galho", "lupa"]
 
-
-"""estrutura1 = (3,[sujeito, verbo,objeto])
-estrutura2 = (2,[sujeito,objeto,verbo])
-estrutura3 = (2,[objeto, sujeito, verbo])
-estrutura4 = (1,[verbo, sujeito, objeto])
-"""
-
-estrutura1 = [sujeito, verbo,objeto]
-estrutura2 = [sujeito,objeto,verbo]
-estrutura3 = [objeto, sujeito, verbo]
-estrutura4 = [verbo, sujeito, objeto]
-
-estruturax = [(3,estrutura1), (2,estrutura2), (2,estrutura3), (1,estrutura4)]
+historia = "eu  fui  ao  barco ontem! pesquei dois peixes e depois de pegar um galho fiz fogo."
 
 
-def valora(arq):
-    for estrutura in estruturax:
-        for sentenca in estrutura:
-            if 
-                print(3)
-            elif 
-                print(2)
-                
-            else
-                print(1)
-                
-valora(historia)
-    
-     
 
-#http://folivetti.github.io/courses/Lcon2016/LCoN2016-RepresentacaoTextual.pdf
-#questão crescente ou descrescente
-# List 
+def responde(item, item2, item3, tree):
+
+    branch = tree.split()
+
+    """for amora in branch:
+         if set(SUJEITO)| set(VERBO) | set(OBJETO)  in branch:
+             print("yuha")
+         else:
+             print("putz")"""
+
+    for amora in item:
+        if amora in branch:
+            print("eba:" + amora )
+
+        else:
+            del branch[amora]
+
+    for amora in item2:
+        if amora in branch:
+            print("eba:" + amora)
+            #print("eba:" and amora)
+
+        else:
+            del branch[amora]
+
+    for amora in item3:
+        if amora in branch:
+            print("eba:" + amora )
+
+        else:
+            del branch[amora]
+
+responde(SUJEITO, VERBO, OBJETO, historia)
