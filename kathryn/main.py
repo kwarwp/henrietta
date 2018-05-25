b937685724d4f0b63e5ff70c0893207bcea19ead
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-25 13:59:38 -0300

    Created kathryn/main.py at Timestamp: 2018-05-25 16:59:38

---
+++ kathryn/main.py
@@ -0,0 +1,45 @@
+# henrietta.kathryn.main.py
+from _spy.vitollino.main import Cena, STYLE, NADA, NoEv
+from _spy.vitollino.main import Texto as Text
+from browser import html
+OCEANO = "https://i.imgur.com/NRi5i6d.jpg"
+STYLE["width"] = 800
+STYLE["height"] = "400px"
+
+
+
+class Texto(Text):
+    def __init__(self, cena=NADA, tit="", txt="", foi=None, **kwargs):
+        super().__init__(cena=cena, tit=tit, txt=txt, foi=foi, **kwargs)
+        #self.elt = Popup.POP.popup
+        self.cena = cena
+        self.area = html.TEXTAREA("xxxxxxxxxx", style=dict(width='100%', resize=None))
+        self.esconde = foi if foi else self.esconde
+        #cena <= self
+
+    def esconde(self, ev=NoEv()):
+        ex.preventDefault()
+        ev.stopPropagation()
+        return False
+        ...
+
+
+    def vai(self, ev=NoEv()):
+        # self.elt = Popup.POP.popup
+        ev.stopPropagation()
+        self.cena.elt <= Popup.POP.popup
+        Popup.POP.tit = self.area
+        Popup.POP.mostra(lambda *_: None, self.tit, self.txt)
+        Popup.POP.esconde = self.esconde
+        return False
+
+class Game:
+    def __init__(self):
+        #self.elt = Popup.POP.popup
+        self.cena = Cena(OCEANO)
+        self.texto = Texto(cena=self.cena, tit="diga,", txt='com q paus a canoa?')
+        self.cena.meio.vai = self.texto.vai
+        #self.cena.vai = self.texto.vai
+        self.cena.vai()
+        
+Game()
