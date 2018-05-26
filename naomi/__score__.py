4d9dcbdb5e005d5a838b8eb2e949e487706a614f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-26 20:04:18 -0300

    Saved naomi/__score__.py at Timestamp: 2018-05-26 23:04:17

--- naomi/__score__.py
+++ naomi/__score__.py
@@ -1,2 +1,4 @@
 {
 "d122c612": {"author_nick": "carlo", "author_name": "carlo", "author_email": "carlo@nce", "author_org": "LABASE", "author_site": "labase", "author_public": "on"},[
+],
+"6562028a": {"author_nick": "carlo", "author_name": "carlo", "author_email": "carlo@nce", "author_org": "LABASE", "author_site": "labase", "author_public": "on"},[
