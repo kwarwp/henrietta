e8ea2ccf323418cecaaf79f2f60598a678e8bcd3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-26 13:19:53 -0300

    Saved kathryn/main.py at Timestamp: 2018-05-26 16:19:53

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,7 +1,7 @@
 # henrietta.kathryn.main.py
 from _spy.vitollino.main import Cena, STYLE, NADA, NoEv
 from _spy.vitollino.main import Texto as Text
-from browser import html, doc
+from browser import html, doc, window
 OCEANO = "https://i.imgur.com/NRi5i6d.jpg"
 STYLE["width"] = 800
 STYLE["height"] = "400px"
@@ -12,10 +12,12 @@ class Texto(Text):
     def __init__(self, cena=NADA, tit="", txt="", texto="..", foi=None, indo=None, **kwargs):
         super().__init__(cena=cena, tit=tit, txt=txt, foi=foi, **kwargs)
         #self.elt = Popup.POP.popup
+        self.t = []
         self.cena = cena
         self.area = html.TEXTAREA(texto, Id="_TEXT_POPUP_", rows=4, style=dict(width='100%', resize=None))
         self.esconde = foi if foi else self.esconde
-        self.area.bind('change', indo) if indo else False
+        self.indo = indo if indo else self.indo
+        self.area.bind('keypress', self.indo)
         #cena <= self
 
     def esconde(self, ev=NoEv()):
@@ -41,17 +43,25 @@ class Texto(Text):
         Popup.POP.mostra(lambda *_: None, self.tit, self.txt)
         Popup.POP.esconde = self.esconde
         return False
+        
+    def sai(self, ev):
+        self.area.value, '¬'.join(self.t)
+        
+    def indo(self, ev):
+        char = ev.keyCode if ev.keyCode else ev.wich
+        self.t.append('{}µ{}'.format(char,str(window.Date.now())[-5:]))
 
 class Game:
     def __init__(self):
         #self.elt = Popup.POP.popup
         self.cena = Cena(OCEANO)
         self.t = []
-        self.texto = Texto(cena=self.cena, tit="diga,", txt='com q paus a canoa?', indo=self.indo,
-        foi=lambda *_:Texto(cena=self.cena, tit="score,", texto=str(self.t)).vai())
+        self.texto = Texto(cena=self.cena, tit="diga,", txt='com q paus a canoa?',
+        foi=lambda *_:Texto(cena=self.cena, tit="score,", texto="\n".join(self.texto.sai())).vai())
         self.cena.meio.vai = self.texto.vai
         #self.cena.vai = self.texto.vai
         self.cena.vai()
+        self.texto.vai()
         
     def indo(self, *_):
         self.t.append(self.texto.area.value)
