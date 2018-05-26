5de34087a3d3376683180c282d6d8f53b9d81719
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-26 13:25:24 -0300

    Saved kathryn/__log__.py at Timestamp: 2018-05-26 13:25:22

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -132,3 +132,23 @@ NameError: name 'Popup' is not defined
     self.cena.elt <= Popup.POP.popup
 NameError: name 'Popup' is not defined
 '''},
+{'date': 'Sat May 26 2018 13:25:21.795 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 188
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 360
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 332
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 349
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 69
+    Game()
+  module <module> line 64
+    self.texto.vai()
+  module <module> line 39
+    self.cena.elt <= Popup.POP.popup
+NameError: name 'Popup' is not defined
+'''},
