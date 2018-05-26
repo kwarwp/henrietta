ec89bef4f62b613d67149bc243fbe7e5f6681c6d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-26 16:46:02 -0300

    Created alexa/__log__.py at Timestamp: 2018-05-26 19:46:02

---
+++ alexa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat May 26 2018 16:46:00.10 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 70
+  print("contagem:", responde(historia,SUJEITO, VERBO, OBJETO)
+                                                                      ^
+SyntaxError: invalid syntax
+'''},
