b0478ba2cc2db5b96cefa23ba0f6e0eb89151730
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-26 09:21:46 -0300

    Created kathryn/__log__.py at Timestamp: 2018-05-26 12:21:46

---
+++ kathryn/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat May 26 2018 09:21:44.431 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 24
+  self._esconde
+  ^
+IndentationError: expected an indented block
+'''},
