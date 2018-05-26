012b2401ac1103a0b0909c3fbedafb035ae3258d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-26 16:37:52 -0300

    Created naomi/__log__.py at Timestamp: 2018-05-26 19:37:52

---
+++ naomi/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat May 26 2018 16:37:50.354 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 51
+  self.pontua(self, pontos):
+                            ^
+SyntaxError: invalid syntax
+'''},
