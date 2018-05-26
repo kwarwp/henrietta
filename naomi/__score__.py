b309ffbc80ef9d7c3a391c6d91b87f1276143bc6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-26 20:53:20 -0300

    Saved naomi/__score__.py at Timestamp: 2018-05-26 23:53:20

--- naomi/__score__.py
+++ naomi/__score__.py
@@ -4,3 +4,4 @@
 "6562028a": {"author_nick": "carlo", "author_name": "carlo", "author_email": "carlo@nce", "author_org": "LABASE", "author_site": "labase", "author_public": "on"},[
 ],
 "05ea2100": {"author_nick": "carlo", "author_name": "carlo", "author_email": "carlo@nce", "author_org": "LABASE", "author_site": "labase", "author_public": "on"},[
+  {'doc_id': 'dc2f57a0', 'carta': '', 'casa': 'https://st.depositphotos.com/1718692/2958/i/950/depositphotos_29580473-stock-photo-stones-and-tree-roots-in.jpg', 'move': 'FLORESTA', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527378800028},
