44101f2f4133c81ee4e4d2030bf84ea5c20157ef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-27 11:23:32 -0300

    Saved kathryn/__log__.py at Timestamp: 2018-05-27 14:23:32

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -291,3 +291,23 @@ NameError: name 'DEBUG' is not defined
     return [pair.split(MU) for pair in datapack.split(PR)]
 AttributeError: 'tuple' object has no attribute 'split'
 '''},
+{'date': 'Sun May 27 2018 11:23:31.321 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 175
+    Game()
+  module <module> line 154
+    self.pontua(self.texto.sai())
+  module <module> line 160
+    grafo = [int(t[2:6]) for _, t in grafo_]
+ValueError: need more than 1 value to unpack
+'''},
