f6e0a0ba40818c3af10b6334fb675bf164b825d5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-27 14:19:21 -0300

    Saved naomi/__score__.py at Timestamp: 2018-05-27 17:19:21

--- naomi/__score__.py
+++ naomi/__score__.py
@@ -28,3 +28,4 @@
   {'doc_id': '30dd6078', 'carta': '', 'casa': 'https://st.depositphotos.com/1718692/2958/i/950/depositphotos_29580473-stock-photo-stones-and-tree-roots-in.jpg', 'move': 'FLORESTA', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527440482768},
   {'doc_id': '30dd6078', 'carta': '', 'casa': 'https://st.depositphotos.com/1718692/2958/i/950/depositphotos_29580473-stock-photo-stones-and-tree-roots-in.jpg', 'move': 'FLORESTA', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527440684956},
   {'doc_id': '30dd6078', 'carta': '', 'casa': 'https://st.depositphotos.com/1718692/2958/i/950/depositphotos_29580473-stock-photo-stones-and-tree-roots-in.jpg', 'move': 'FLORESTA', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527440936003},
+  {'doc_id': '30dd6078', 'carta': '', 'casa': 'https://st.depositphotos.com/1718692/2958/i/950/depositphotos_29580473-stock-photo-stones-and-tree-roots-in.jpg', 'move': 'FLORESTA', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527441560797},
