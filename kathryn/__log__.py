3437208109c3c1a2e993aab52b5ae18a64568081
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-27 11:36:56 -0300

    Saved kathryn/__log__.py at Timestamp: 2018-05-27 14:36:55

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -377,3 +377,25 @@ AttributeError: 'tuple' object has no attribute 'split'
     return [pair.split(MU) for pair in datapack.split(PR)]
 AttributeError: 'tuple' object has no attribute 'split'
 '''},
+{'date': 'Sun May 27 2018 11:36:54.979 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 176
+    Game()
+  module <module> line 154
+    self.pontua(self.texto.sai())
+  module <module> line 159
+    grafo_ = Plotter.unpack(pontos)
+  module <module> line 83
+    return [pair.split(MU) for pair in datapack.split(PR)]
+AttributeError: 'tuple' object has no attribute 'split'
+'''},
