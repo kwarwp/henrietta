7a5ffe7a68e5dcb2ef5bb9132d35f84b80db1bb9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-30 12:02:19 -0300

    Saved stacy/__score__.py at Timestamp: 2018-05-30 15:02:16

--- stacy/__score__.py
+++ stacy/__score__.py
@@ -80,3 +80,4 @@
   {'doc_id': '9b1d2e2a', 'carta': 303, 'casa': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW0BY3YH52d4HOogtzl4XDXyawkVJeAi-5pNTHvhQZBrB-OF2i', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527692530643},
   {'doc_id': '9b1d2e2a', 'carta': 404, 'casa': 'https://images.vexels.com/media/users/3/148533/list/ab32e2a639adb5969b49b6420daf0514-boys-band-cartoon-illustration.jpg', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527692530866},
   {'doc_id': '9b1d2e2a', 'carta': 505, 'casa': 'https://cdna.artstation.com/p/assets/images/images/004/585/984/large/kamila-redkiewicz-museum.jpg?1484770586', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527692531124},
+  {'doc_id': '9b1d2e2a', 'carta': 505, 'casa': 'https://image.freepik.com/vettori-gratuito/interno-cucina-moderna-piatto_6280-115.jpg ', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527692531398},
