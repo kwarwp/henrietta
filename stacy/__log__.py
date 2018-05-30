70842edd818a17d9e1dbb30b6794d0b833c94933
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-30 11:50:42 -0300

    Saved stacy/__log__.py at Timestamp: 2018-05-30 14:50:42

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -35,3 +35,21 @@ AttributeError: 'NoneType' object has no attribute 'vai'
     _salas[sala].elemento(**design)
 AttributeError: 'Cena' object has no attribute 'elemento'
 '''},
+{'date': 'Wed May 30 2018 11:50:37.675 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 160
+    Templo()
+  module <module> line 84
+    _design =[
+AttributeError: 'Templo' object has no attribute 'vai'
+'''},
