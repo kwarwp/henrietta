9947d4c95667fe485f58e45e0485ff9a420a0157
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-30 11:41:28 -0300

    Saved stacy/__score__.py at Timestamp: 2018-05-30 14:41:28

--- stacy/__score__.py
+++ stacy/__score__.py
@@ -1,1 +1,2 @@
   {'doc_id': '9b1d2e2a', 'carta': 202, 'casa': 'https://i.imgur.com/NRi5i6d.jpg', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527691286723},
+  {'doc_id': '9b1d2e2a', 'carta': 303, 'casa': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW0BY3YH52d4HOogtzl4XDXyawkVJeAi-5pNTHvhQZBrB-OF2i', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527691286890},
