3360598b5c8ab3cba67705ad2733edfde59e4e12
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-30 18:43:42 -0300

    Saved stacy/__score__.py at Timestamp: 2018-05-30 21:43:42

--- stacy/__score__.py
+++ stacy/__score__.py
@@ -399,3 +399,4 @@
   {'doc_id': '12e73691', 'carta': 505, 'casa': 'https://img3.stockfresh.com/files/d/ddraw/m/72/2405156_stock-photo-sport-characters-with-background.jpg', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527716601255},
   {'doc_id': '12e73691', 'carta': 606, 'casa': 'https://image.freepik.com/vettori-gratuito/interno-cucina-moderna-piatto_6280-115.jpg ', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1527716602076},
     {'doc_id': '12e73691', 'carta': 'folha_1_1', 'casa': (1, 1), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_0_0', '_level': 2, 'tempo': 1527716619785},
+    {'doc_id': '12e73691', 'carta': 'folha_0_1', 'casa': (0, 1), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_1_1', '_level': 2, 'tempo': 1527716622132},
