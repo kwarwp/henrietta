9c68290c3137c3ec82175916d3e3d4b564cc4695
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 12:00:43 -0300

    Saved angie/__log__.py at Timestamp: 2018-06-07 15:00:42

--- angie/__log__.py
+++ angie/__log__.py
@@ -38,3 +38,19 @@ NameError: name 'corredor' is not defined
     corredor.entra()
 NameError: name 'corredor' is not defined
 '''},
+{'date': 'Thu Jun 07 2018 11:57:12.852 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    corredor = corredor.entra()
+NameError: name 'corredor' is not defined
+'''},
