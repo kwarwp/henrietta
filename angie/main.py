dd4f6fd779c7659fbb3ba62b6fddb98e20272366
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 11:50:31 -0300

    Saved angie/main.py at Timestamp: 2018-06-07 14:50:31

--- angie/main.py
+++ angie/main.py
@@ -14,13 +14,31 @@ class Templo:
 class EntradaDoTemplo(Templo):
     def __init__(self):
         super().__init__()
+        self.corredor = CorredorDoTemplo()
+    def sai(self):
+        Templo.sai(self)
+        self.corredor.entra()
+        
+class CorredorDoTemplo(Templo):
+    def __init__(self):
+        super().__init__()
+        
+class SalaDoTemplo(Templo):
+    def __init__(self):
+        super().__init__()
+        
+        
+
    
         
-musica = Templo()
-oceano = Templo()
-floresta = Templo()
-floresta.entra()
-oceano.entra()
+musica = SalaDoTemplo()
+oceano = SalaDoTemplo()
+floresta = SalaDoTemplo()
+#floresta.entra()
+#oceano.entra()
 entrada = EntradaDoTemplo()
 entrada.entra()
-print("musica:{}, oceano:{}, floresta:{}, entrada:{}".format(musica.entrou(), oceano.entrou(), floresta.entrou(), entrada.entrou()))
+corredor = CorredorDoTemplo()
+
+
+print("musica:{}, oceano:{}, floresta:{}, entrada:{}, corredor:{}".format(musica.entrou(), oceano.entrou(), floresta.entrou(), entrada.entrou(), corredor.entrou()))
