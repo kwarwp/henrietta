81e483355f19ef32b46533f7d927aafee3fb1323
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 11:28:36 -0300

    Saved angie/main.py at Timestamp: 2018-06-07 14:28:35

--- angie/main.py
+++ angie/main.py
@@ -1,1 +1,26 @@
 # henrietta.angie.main.py
+class Templo:
+    def __init__(self):
+        self._esta_no_templo = False
+    def entra(self):
+        self._esta_no_templo = True
+        
+    def sai(self):
+        self._esta_no_templo = False
+        
+    def entrou(self):
+        return self._esta_no_templo 
+        
+class EntradaDoTemplo(Templo):
+    def __init__(self):
+        super().__init__()
+   
+        
+musica = Templo()
+oceano = Templo()
+floresta = Templo()
+floresta.entra()
+oceano.entra()
+entrada = EntradaDoTemplo()
+entrada.entra()
+print("musica:{}, oceano:{}, floresta:{}, entrada:{}".format(musica.entrou(), oceano.entrou(), floresta.entrou(), entrada.entrou()))
