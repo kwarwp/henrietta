6e5fc68a2bc192ae543a4cb5f9263af4b7f932b2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 11:08:03 -0300

    Created courtney/__log__.py at Timestamp: 2018-06-07 14:08:02

---
+++ courtney/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Jun 07 2018 11:10:30.847 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 20
+  print("musica:{}, oceano:{}, floresta:{}".format(musica.templo(), oceano.templo(), floresta.templo())
+       ^
+SyntaxError: Unbalanced bracket (
+'''},
