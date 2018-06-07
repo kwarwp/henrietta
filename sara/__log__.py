5afea15535aaf147ccf966273fee03c1d951e55c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 11:37:55 -0300

    Saved sara/__log__.py at Timestamp: 2018-06-07 14:37:55

--- sara/__log__.py
+++ sara/__log__.py
@@ -113,3 +113,19 @@ NameError: name 'entrada' is not defined
     print("musica:{}, oceano:{}, floresta:{}".format(musica.entrou(), oceano.entrou(), floresta.entrou(), entrada.entrou()))
 NameError: name 'entrada' is not defined
 '''},
+{'date': 'Thu Jun 07 2018 11:40:55.175 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 25
+    corredor = CorredorDoTemplo()
+NameError: name 'CorredorDoTemplo' is not defined
+'''},
