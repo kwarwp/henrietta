1ebfab37a2c01cf13a36a4eeec9c00b7ce78729b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 11:55:58 -0300

    Saved sara/__log__.py at Timestamp: 2018-06-07 14:55:58

--- sara/__log__.py
+++ sara/__log__.py
@@ -129,3 +129,19 @@ NameError: name 'entrada' is not defined
     corredor = CorredorDoTemplo()
 NameError: name 'CorredorDoTemplo' is not defined
 '''},
+{'date': 'Thu Jun 07 2018 11:58:57.666 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 38
+    print("musica:{}, oceano:{}, floresta:{}, entrada:{}, cd:{}".format(
+IndexError: list index out of range
+'''},
