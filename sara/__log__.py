8a343100072ccc731d53e909b7c16a9149df6a21
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 11:24:43 -0300

    Saved sara/__log__.py at Timestamp: 2018-06-07 14:24:43

--- sara/__log__.py
+++ sara/__log__.py
@@ -63,3 +63,21 @@ TypeError: format() takes 2 positional argument but more were given
     print("musica:{}, oceano:{}, floresta:{}",format(
 TypeError: format() takes 2 positional argument but more were given
 '''},
+{'date': 'Thu Jun 07 2018 11:27:42.677 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    print("musica:{}, oceano:{}, floresta:{}".format(
+  module <module> line 12
+    return self._esta_no_templo
+AttributeError: 'Templo' object has no attribute '_esta_no_templo'
+'''},
