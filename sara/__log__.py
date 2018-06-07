04ca1fe907d218d6db26e4b03fc19ef714d18c26
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 12:04:26 -0300

    Saved sara/__log__.py at Timestamp: 2018-06-07 15:04:26

--- sara/__log__.py
+++ sara/__log__.py
@@ -177,3 +177,22 @@ IndexError: list index out of range
     print("musica:{}, oceano:{}, floresta:{}, entrada: {}, cd:{}".format(musica.entrou(), oceano.entrou(), floresta.entrou(), entrada.entrou()))
 IndexError: list index out of range
 '''},
+{'date': 'Thu Jun 07 2018 12:07:25.795 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''musica:False, oceano:False, floresta:False, entrada: True, cd:False
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 47
+    entrada.sai()
+  module <module> line 20
+    self.corredor.entra()
+TypeError: entra() missing 1 positional argument: self
+'''},
