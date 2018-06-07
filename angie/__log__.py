1bbd7aed285d18a61f1a79fd5b9ee6bc75d4b928
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 11:23:01 -0300

    Created angie/__log__.py at Timestamp: 2018-06-07 14:23:01

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Jun 07 2018 11:19:30.392 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  class EntradaDoTemplo(Templo)
+                                ^
+SyntaxError: invalid syntax
+'''},
