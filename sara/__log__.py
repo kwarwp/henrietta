135800c68d2ce9d1af747490a4e475f6cca35ff1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 11:35:12 -0300

    Saved sara/__log__.py at Timestamp: 2018-06-07 14:35:12

--- sara/__log__.py
+++ sara/__log__.py
@@ -81,3 +81,19 @@ TypeError: format() takes 2 positional argument but more were given
     return self._esta_no_templo
 AttributeError: 'Templo' object has no attribute '_esta_no_templo'
 '''},
+{'date': 'Thu Jun 07 2018 11:38:11.890 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    print("musica:{}, oceano:{}, floresta:{}".format(
+NameError: name 'entrada' is not defined
+'''},
