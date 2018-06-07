e4d505cb915d1b7c08d88ed21b4960b4b089568d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 12:04:56 -0300

    Created sara/main.py at Timestamp: 2018-06-07 15:04:55

---
+++ sara/main.py
@@ -0,0 +1,48 @@
+# henrietta.sara.main.py
+class Templo:
+    def __init__ (self):
+        self._esta_no_templo = False
+    def entra(self):
+        self._esta_no_templo = True
+        
+    def sai(self):
+        self._esta_no_templo = False
+        
+    def entrou(self):
+        return self._esta_no_templo
+
+class EntradaDoTemplo(Templo):
+    def __init__(self):
+        super().__init__()
+        self.corredor = CorredorDoTemplo
+    def sai(self):
+        Templo.sai(self)
+        self.corredor.entra()
+        
+class CorredorDoTemplo(Templo):
+    def __init__(self):
+        super().__init__()
+        
+class SalaDoTemplo(Templo):
+    def __init__(self):
+        super().__init__()
+
+def mostra_templo():
+    print("musica:{}, oceano:{}, floresta:{}, entrada: {}, cd:{}".format(
+        musica.entrou(), oceano.entrou(),
+        floresta.entrou(), entrada.entrou(), corredor.entrou()
+        )
+    )
+    
+musica =  SalaDoTemplo()
+oceano =  SalaDoTemplo()
+floresta =  SalaDoTemplo()
+#floresta.entra()
+#oceano.entra()
+entrada = EntradaDoTemplo()
+corredor = entrada.corredor()
+entrada.entra()
+mostra_templo()
+
+entrada.sai()
+mostra_templo()
