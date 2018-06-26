a3e3a365b563e1a72a1841aa526c84f0e4485f17
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-26 19:52:17 -0300

    Saved adda/main.py at Timestamp: 2018-06-26 22:52:17

--- adda/main.py
+++ adda/main.py
@@ -6,7 +6,7 @@ STYLE['width'] = 699
 TutorialInterativo = "https://i.imgur.com/tfatd8y.png"
 Cena_direita = "https://i.imgur.com/2YX3bLb.png"
 Cena_esquerda = "https://i.imgur.com/NC0Sj4z.png"
-
+comousarelemento = "https://i.imgur.com/amn4taQ.png"
 class CenaTutorialInterativo():
   def __init__(self):
     
@@ -38,6 +38,27 @@ class CenaTutorialInterativo():
 def vai_CenaTutorialInterativo():
   cenaImporta = CenaTutorialInterativo()
   cenaImporta.vai()
-
+ 
 if __name__ == "__main__":
 	vai_CenaTutorialInterativo()
+    
+     
+  from _spy.vitollino.main import Cena, Texto, Elemento, STYLE
+from _spy.vitollino.main import INVENTARIO as inv
+
+FLORESTA = "https://st.depositphotos.com/1718692/2958/i/950/depositphotos_29580473-stock-photo-stones-and-tree-roots-in.jpg"
+TRANSPARENTE = "http://1.bp.blogspot.com/-eK24sreQNsg/Uvy1AT5iVSI/AAAAAAAAAGo/TRHh_nkqhVY/s1600/fundo-blog.png"
+class Estados:
+    def __init__(self):
+        floresta = Cena(FLORESTA)
+        self.fantasma = Cena()
+        floresta.vai()
+        self.galhos = gag = Elemento(img=TRANSPARENTE,tit="galhos", style=dict(
+            left=28, top=130, width=60, height="60px"))
+        gag.entra(floresta)
+        gag.vai 
+        
+        
+if __name__ == "__main__":
+    Estados()
+
