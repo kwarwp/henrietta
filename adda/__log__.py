e5c223a3e7d735eaaf64213832bc3915917e1bc7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-27 11:41:34 -0300

    Created adda/__log__.py at Timestamp: 2018-06-27 14:41:34

---
+++ adda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Jun 27 2018 11:41:32.84 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 46
+  from _spy.vitollino.main import Cena, Texto, Elemento, STYLE
+  ^
+IndentationError: unexpected indent
+'''},
