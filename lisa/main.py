f0b6b91c94c97b191ab6615401b38a6caec6b41a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-18 15:12:04 -0300

    Saved lisa/main.py at Timestamp: 2018-07-18 18:12:04

--- lisa/main.py
+++ lisa/main.py
@@ -1,27 +1,25 @@
 # henrietta.lisa.main.py
 from _spy.vitollino.main import Cena, STYLE
 
-STYLE ['width'] = 699
+STYLE ['width'] = 700
 
-R_OESTE = "https://i.imgur.com/XJXjA9r.jpg"
-R_LESTE = "https://i.imgur.com/rHzbmtM.jpg"
-R_NORTE = "https://i.imgur.com/IPa06hM.jpg"
+dino_direita = "https://i.imgur.com/P6nIihH.jpg"
+dino_esquerda = "https://i.imgur.com/P54FLEU.jpg"
 
-class CenaRecepcao():
+class CenaDino():
   def __init__(self):
-    self.cena_norte = Cena(img = R_NORTE, direita=self.cena_leste)
-    self.cena_leste = Cena(img = R_LESTE, esquerda=self.cena_norte)
-    self.cena_oeste = Cena(img = R_OESTE, direita=self.cena_norte)
+    self.cena_direita = Cena(img = dino_direita)
+    self.cena_esquerda = Cena(dino_esquerda, direita = self.cena_direita)    
+    self.cena_direita.esquerda = self.cena_esquerda
+    self.cena_direita.direita = self.cena_esquerda
+    self.cena_esquerda.direita = self.cena_direita
+    self.cena_esquerda.esquerda = self.cena_direita
     
-    self.cena_norte.direita = self.cena_leste
-    self.cena_leste.esquerda = self.cena_norte
-    self.cena_norte.esquerda = self.cena_oeste
-    self.cena_oeste.direita = self.cena_norte
     
+    
+def sala():
+  cena_dino = CenaDino()
+  cena_dino.vai()
   
-  def vai_Recepcao():
-      cenaImporta = CenaRecepcao
-      cenaImporta.vai()
- 
-if __name__ == "__main__":
-	vai_CenaRecepcao()
+  if __name__ == "__main__":
+  	sala()
