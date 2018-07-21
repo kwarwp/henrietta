454a3ef44c9d4f7b713b19a3e2127488cf83b8c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-20 23:07:59 -0300

    Saved kellee/__log__.py at Timestamp: 2018-07-21 02:07:58

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -6,3 +6,10 @@
                                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Jul 21 2018 23:07:49.624 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  lado1_e = Elemento(img=ILUMINATTI, tit'O universo nÃÂÂÂ£o gira em torno de vocÃÂÂÂª nÃÂÂÂ£o,bb', style = dict(left= 150, top=100, width=60, height=200, bottom=100))
+                                         ^
+SyntaxError: invalid syntax
+'''},
