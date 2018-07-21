87a9fb76365db11be2c367884e373a5f79aa21ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-20 23:07:27 -0300

    Created kellee/__log__.py at Timestamp: 2018-07-21 02:07:27

---
+++ kellee/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat Jul 21 2018 23:07:16.279 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  inicioe = Elemento(img=MACA, tit"Eis que a gravidade aparece", style = dict(left= 150,top=100, width=60, height=200,bottom=100))    
+                                   ^
+SyntaxError: invalid syntax
+'''},
