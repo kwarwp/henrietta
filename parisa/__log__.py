0b0e28641f99349895505f75fcc076a361d97c01
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-20 23:31:14 -0300

    Saved parisa/__log__.py at Timestamp: 2018-07-21 02:31:14

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -6,3 +6,10 @@
     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Jul 21 2018 23:31:05.557 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 152
+  self.troll = Elemento(trf, style=dict(left=10, top=100)
+                                                                   ^
+SyntaxError: invalid syntax
+'''},
