835b37de878294beec76fb3ee98774bcd7a08a38
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-03 11:00:18 -0300

    Saved rachel/__score__.py at Timestamp: 2018-10-03 14:00:16

--- rachel/__score__.py
+++ rachel/__score__.py
@@ -109,3 +109,4 @@
   {'doc_id': '261e5cff', 'carta': 606, 'casa': 'https://img3.stockfresh.com/files/d/ddraw/m/72/2405156_stock-photo-sport-characters-with-background.jpg', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1538551317703},
     {'doc_id': '261e5cff', 'carta': 'folha_0_0', 'casa': (0, 0), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_0_1', '_level': 2, 'tempo': 1538552980090},
     {'doc_id': '261e5cff', 'carta': 'folha_1_0', 'casa': (1, 0), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_1_0', '_level': 2, 'tempo': 1538552989171},
+    {'doc_id': '261e5cff', 'carta': 'folha_0_1', 'casa': (0, 1), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_0_0', '_level': 2, 'tempo': 1538552996811},
