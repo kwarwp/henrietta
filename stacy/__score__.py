637ffa33fc5bfdf8b28590b3c6e2a81f21a083b0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 10:20:38 -0300

    Saved stacy/__score__.py at Timestamp: 2018-10-22 13:20:38

--- stacy/__score__.py
+++ stacy/__score__.py
@@ -524,3 +524,4 @@
   {'doc_id': '23ce8835', 'carta': 606, 'casa': 'https://image.freepik.com/vettori-gratuito/interno-cucina-moderna-piatto_6280-115.jpg ', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1531939783098},
     {'doc_id': '23ce8835', 'carta': 'folha_0_0', 'casa': (0, 0), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_0_0', '_level': 2, 'tempo': 1531939802321},
     {'doc_id': '23ce8835', 'carta': 'folha_0_1', 'casa': (0, 1), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_2_1', '_level': 2, 'tempo': 1531939809740},
+  {'doc_id': '7bc0bbbf', 'carta': 202, 'casa': 'https://i.imgur.com/NRi5i6d.jpg', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1540213957763},
