90589cf152b53bc275e0ff2189838585b3010cbc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 10:20:39 -0300

    Saved stacy/__score__.py at Timestamp: 2018-10-22 13:20:39

--- stacy/__score__.py
+++ stacy/__score__.py
@@ -525,3 +525,4 @@
     {'doc_id': '23ce8835', 'carta': 'folha_0_0', 'casa': (0, 0), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_0_0', '_level': 2, 'tempo': 1531939802321},
     {'doc_id': '23ce8835', 'carta': 'folha_0_1', 'casa': (0, 1), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_2_1', '_level': 2, 'tempo': 1531939809740},
   {'doc_id': '7bc0bbbf', 'carta': 202, 'casa': 'https://i.imgur.com/NRi5i6d.jpg', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1540213957763},
+  {'doc_id': '7bc0bbbf', 'carta': 303, 'casa': 'https://images.vexels.com/media/users/3/148533/list/ab32e2a639adb5969b49b6420daf0514-boys-band-cartoon-illustration.jpg', 'move': 'BLOCO', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1540213958076},
