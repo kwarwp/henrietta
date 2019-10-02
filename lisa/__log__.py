e6f9ff9368f712113bf6321035e88a8553b17b53
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 13:53:29 -0300

    Created lisa/__log__.py @2019-10-02 16:53

---
+++ lisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Oct 02 2019 13:53:26.895 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 24
+  def vai_CenaRecepcao():
+  ^
+IndentationError: unexpected indent
+'''},
