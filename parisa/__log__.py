5cab9c50f5a891a24c70e0874751ed533e682aa4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-03 10:51:40 -0300

    Saved parisa/__log__.py @2019-10-03 13:51

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -55,3 +55,10 @@ SyntaxError: invalid syntax
     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Oct 04 2019 10:51:33.684 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 152
+  self.troll = Elemento(trf, style=dict(left=10, top=100)
+                                                                   ^
+SyntaxError: invalid syntax
+'''},
