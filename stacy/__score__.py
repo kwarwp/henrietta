f3c53020c3000e74d5acfbaeaba05241a185be6e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-09 18:05:37 -0300

    Saved stacy/__score__.py @2019-10-09 21:05

--- stacy/__score__.py
+++ stacy/__score__.py
@@ -600,3 +600,4 @@
     {'doc_id': '0e0d4332', 'carta': 'folha_1_1', 'casa': (1, 2), 'move': 'DROP', 'ponto': 150, 'valor': 'casa_0_2', '_level': 2, 'tempo': 1570655109588},
     {'doc_id': '0e0d4332', 'carta': 'folha_2_1', 'casa': (2, 2), 'move': 'DROP', 'ponto': 150, 'valor': 'casa_3_2', '_level': 2, 'tempo': 1570655116402},
     {'doc_id': '0e0d4332', 'carta': 'folha_3_1', 'casa': (3, 2), 'move': 'DROP', 'ponto': 150, 'valor': 'casa_2_2', '_level': 2, 'tempo': 1570655124538},
+    {'doc_id': '0e0d4332', 'carta': 'folha_0_0', 'casa': (0, 1), 'move': 'DROP', 'ponto': 150, 'valor': 'casa_3_3', '_level': 2, 'tempo': 1570655136503},
