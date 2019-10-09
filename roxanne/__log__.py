174f68572a9d68642b646c4b198fba8e6cfee435
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-09 18:02:07 -0300

    Created roxanne/__log__.py @2019-10-09 21:02

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Oct 09 2019 18:02:04.890 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 35
+  self.cena_o.vai()
+  ^
+IndentationError: expected an indented block
+'''},
