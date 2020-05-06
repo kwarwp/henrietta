3242b7fe09954ce808f4c2f4ed6176d3dc3b051a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-06 04:57:14 -0300

    Saved sara/__log__.py @2020-05-06 07:57

--- sara/__log__.py
+++ sara/__log__.py
@@ -215,3 +215,22 @@ Traceback (most recent call last):
     self.corredor.entra()
 TypeError: entra() missing 1 positional argument: self
 '''},
+{'date': 'Wed May 06 2020 04:57:12.937 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''musica:False, oceano:False, floresta:False, entrada: True, cd:False
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 47
+    entrada.sai()
+  module <module> line 20
+    self.corredor.entra()
+TypeError: entra() missing 1 positional argument: self
+'''},
