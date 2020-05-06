df0ce2e8acaabe1f6298ce2f543a26cc7dbf30e5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-06 05:01:47 -0300

    Saved rachel/__score__.py @2020-05-06 08:01

--- rachel/__score__.py
+++ rachel/__score__.py
@@ -141,3 +141,4 @@
     {'doc_id': '9130f3f6', 'carta': 'folha_0_1', 'casa': (0, 1), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_1_0', '_level': 2, 'tempo': 1588752100555},
     {'doc_id': '9130f3f6', 'carta': 'folha_1_1', 'casa': (1, 1), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_1_1', '_level': 2, 'tempo': 1588752102850},
     {'doc_id': '9130f3f6', 'carta': 'folha_0_0', 'casa': (0, 0), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_0_1', '_level': 2, 'tempo': 1588752105215},
+    {'doc_id': '9130f3f6', 'carta': 'folha_1_0', 'casa': (1, 0), 'move': 'DROP', 'ponto': 200, 'valor': 'casa_0_0', '_level': 2, 'tempo': 1588752106786},
