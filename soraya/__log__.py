a295c5b9edaa2e1b6ed0b889d5c15c28d673b80d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 12:22:30 -0300

    Saved soraya/__log__.py @2020-09-27 15:22

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -28,3 +28,25 @@ SyntaxError: invalid syntax
     INVENTARIO.score(
 TypeError: score() got an unexpected keyword argument '_level'
 '''},
+{'date': 'Sun Sep 27 2020 12:22:29.401 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 180
+    Puzzle()
+  module <module> line 172
+    self.puzzle = Bloco(oce, 2, 2, style=dict(left=10, top=100))
+  module <module> line 108
+    self.img = img
+  module _spy.vitollino.main line 667
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Bloco' object has no attribute 'elt'
+'''},
