a0e8df3bcc36e64203d1d241e0e849e3fbae81cf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 12:20:33 -0300

    Saved stacy/__log__.py @2020-09-27 15:20

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -131,3 +131,27 @@ NameError: name 'kwargs' is not defined
     self.tsala = Texto(self.tabuleiro, "Monte o quebra cabeca e entre na sala.")
 NameError: name 'Texto' is not defined
 '''},
+{'date': 'Sun Sep 27 2020 12:20:32.980 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 215
+    Templo()
+  module <module> line 83
+    _salas = {"salatemplo{}".format(n+1): SalaTemplo(tema, x, y) for n, (tema, x, y) in enumerate([
+  module <module> line 170
+    quebra_cabeca = Bloco(tema, x, y, vai=destrava_templo)
+  module soraya.main line 108
+    self.img = img
+  module _spy.vitollino.main line 667
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Bloco' object has no attribute 'elt'
+'''},
