1f3a5211dd0307592104519fd85d993e5f5658d6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-11-08 14:33:10 -0300

    Saved adda/__log__.py @2022-11-08 17:33

--- adda/__log__.py
+++ adda/__log__.py
@@ -6,3 +6,9 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Nov 08 2022 14:33:09.844 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: Cannot read properties of null (reading '0')
+  module '$exec_320' line 2
+from _spy..main import Cena
+'''},
