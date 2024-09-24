6e87598f0c3004e3f085d31f721ab3f3eec37a48
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 18:41:23 -0300

    Saved samantha/__log__.py @2024-09-24 21:41

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -18,3 +18,22 @@ Traceback (most recent call last):
     del branch[amora]
 TypeError: list indices must be integer, not <undefined>
 '''},
+{'date': 'Tue Sep 24 2024 18:41:22.427 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''eba:eu
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 42
+    responde(SUJEITO, VERBO, OBJETO, historia)
+  module <module> line 25
+    del branch[amora]
+TypeError: list indices must be integer, not <undefined>
+'''},
